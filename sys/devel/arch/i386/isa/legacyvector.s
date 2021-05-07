1372fcc0e18d89192615b14bdd95772632997914
TheSledgeHammer <martin.kelly4000@gmail.com>
2021-05-08 00:22:34 +1000

    Update legacyvector.s

--- sys/devel/arch/i386/isa/legacyvector.s
+++ sys/devel/arch/i386/isa/legacyvector.s
@@ -1,3 +1,44 @@
+/*-
+ * Copyright (C) 1989, 1990 W. Jolitz
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	$Id: vector.s,v 1.18.2.1 1994/10/11 09:46:40 mycroft Exp $
+ */
+
+#include "assym.h"
+
 #include <i386/isa/icu.h>
 #include <dev/isa/isareg.h>
 #include <dev/isa/isavar.h>
@@ -26,8 +67,8 @@
 #define	ENABLE_ICU1_AND_2(irq_num) 											 \
 		movb	$(0x60|2),%al					/* specific EOI for IRQ2 */	;\
 		outb	%al,$IO_ICU1
-#define	MASK(irq_num, icu)
-#define	UNMASK(irq_num, icu) 												 \
+#define	LEGACY_MASK(irq_num, icu)
+#define	LEGACY_UNMASK(irq_num, icu) 										 \
 		movb	$(0x60|(irq_num%8)),%al			/* specific EOI */			;\
 		outb	%al,$icu
 
@@ -71,8 +112,8 @@
 
 #else /* ICU_HARDWARE_MASK */
 
-#define	MASK(irq_num, icu)
-#define	UNMASK(irq_num, icu)
+#define	LEGACY_MASK(irq_num, icu)
+#define	LEGACY_UNMASK(irq_num, icu)
 
 #endif /* ICU_HARDWARE_MASK */
 #endif /* ICU_SPECIAL_MASK_MODE */
@@ -83,7 +124,6 @@
 #define	XHOLD(name, irq_num)		_Xname_/**/hold/**/irq_num
 #define	XSTRAY(name, irq_num)		_Xname_/**/stray/**/irq_num
 
- */
  /*
  * Normal vectors.
  *
