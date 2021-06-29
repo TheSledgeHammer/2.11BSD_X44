017b3d34bc63fff676d7ca5908a65317d1193cbc
TheSledgeHammer <martin.kelly4000@gmail.com>
2021-06-29 19:43:51 +1000

    added byacc

---
+++ contrib/byacc/dist/test/btyacc/err_syntax11.tab.h
@@ -0,0 +1,7 @@
+/*	$NetBSD: err_syntax11.tab.h,v 1.1.1.3 2016/01/09 21:59:46 christos Exp $	*/
+
+#ifndef _err_syntax11__defines_h_
+#define _err_syntax11__defines_h_
+
+
+#endif /* _err_syntax11__defines_h_ */
