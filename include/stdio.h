d276a0d2c7377a21362182b7c76b843340b67305
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2021-08-24 16:52:14 +1000

    Update stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -251,10 +251,10 @@ int	 	feof (FILE *);
 int	 	ferror (FILE *);
 int	 	fflush (FILE *);
 int	 	fgetc (FILE *);
-int	    fgetpos(FILE * __restrict, fpos_t * __restrict)
+int	    fgetpos(FILE * __restrict, fpos_t * __restrict);
 char	*fgets(char * __restrict, int, FILE * __restrict);
 FILE	*fopen(const char * __restrict , const char * __restrict);
-int	    fprintf(FILE * __restrict, const char * __restrict, ...)
+int	    fprintf(FILE * __restrict, const char * __restrict, ...);
 int	 	fputc (int, FILE *);
 int	    fputs(const char * __restrict, FILE * __restrict);
 size_t	fread(void * __restrict, size_t, size_t, FILE * __restrict);
@@ -282,14 +282,14 @@ void 	rewind (FILE *);
 int	 	scanf (const char * __restrict, ...);
 void	setbuf(FILE * __restrict, char * __restrict);
 int	    setvbuf(FILE * __restrict, char * __restrict, int, size_t);
-int	    sscanf(const char * __restrict, const char * __restrict, ...)
+int	    sscanf(const char * __restrict, const char * __restrict, ...);
 int	    sprintf(char * __restrict, const char * __restrict, ...);
 FILE 	*tmpfile (void);
 char 	*tmpnam (char *);
 int	 	ungetc (int, FILE *);
 int	    vfprintf(FILE * __restrict, const char * __restrict, __va_list);
-int	    vprintf(const char * __restrict, __va_list)
-int	    vsprintf(char * __restrict, const char * __restrict, __va_list)
+int	    vprintf(const char * __restrict, __va_list);
+int	    vsprintf(char * __restrict, const char * __restrict, __va_list);
 __END_DECLS
 
 /*
@@ -333,7 +333,7 @@ int	 	setlinebuf (FILE *);
 char	    *tempnam (const char *, const char *);
 int	    snprintf(char * __restrict, size_t, const char * __restrict, ...);
 int	    vsnprintf(char * __restrict, size_t, const char * __restrict, __va_list);
-int	    vasprintf(char ** __restrict, const char * __restrict, __va_list)
+int	    vasprintf(char ** __restrict, const char * __restrict, __va_list);
 int	    vscanf(const char * __restrict, __va_list);
 int	    vsscanf(const char * __restrict, const char * __restrict, __va_list);
 __END_DECLS
