36995e4f2d1985c91c62dd5a083d86848907e044
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2022-04-22 11:27:37 +1000

    updated map.h

--- sys/sys/map.h
+++ sys/sys/map.h
@@ -9,6 +9,7 @@
 #define	 _SYS_MAP_H_
 
 #include <vm/include/vm.h>
+
 /*
  * Resource Allocation Maps.
  *
@@ -67,14 +68,14 @@ struct vmmapent {
 };
 
 #ifdef _KERNEL
-struct map *coremap[1];																	/* space for core allocation */
-struct map *swapmap[1];																	/* space for swap allocation */
+extern struct map coremap[1];																	/* space for core allocation */
+extern struct map swapmap[1];																	/* space for swap allocation */
 int	nswapmap;
 
-void	rmapinit(void);
-long 	rmalloc(struct map *mp, long size); 											/* Allocate units from the given map. */
-void 	rmfree(struct map *mp, long size, long addr); 									/* Free the previously allocated units at addr into the specified map.*/
-long 	rmalloc3(struct map *mp, long d_size, long s_size, long u_size, long a[3]);		/* Allocate resources for the three segments of a process.*/
-void	rminit(struct map *mp, long size, long addr, char *name, int mapsize);			/* Initialized resource malloc */
+void	    rmapinit(void);
+memaddr_t   rmalloc(struct map *, size_t); 											/* Allocate units from the given map. */
+void 	    rmfree(struct map *, size_t, memaddr_t); 									/* Free the previously allocated units at addr into the specified map.*/
+memaddr_t 	rmalloc3(struct map *, size_t, size_t, size_t, memaddr_t *);		/* Allocate resources for the three segments of a process.*/
+void	    rminit(struct map *, memaddr_t, size_t, char *name, int, int);			/* Initialized resource malloc */
 #endif
 #endif /* _SYS_MAP_H_ */
