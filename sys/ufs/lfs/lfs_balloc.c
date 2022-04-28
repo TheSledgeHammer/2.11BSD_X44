bc8ca48be9d4ddd6d116bdf2a58e63e86a79643d
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2022-04-28 21:34:19 +1000

    lfs_balloc fixes

--- sys/ufs/lfs/lfs_balloc.c
+++ sys/ufs/lfs/lfs_balloc.c
@@ -118,7 +118,7 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 					brelse(ibp);
 					return(ENOSPC);
 				} else {
-					DIP_SET(ip, blocks, DIP(ip, blocks) += bb);//ip->i_ffs1_blocks += bb;
+					DIP_SET(ip, blocks, DIP(ip, blocks) + bb);//ip->i_ffs1_blocks += bb;
 					ip->i_lfs->lfs_bfree -= bb;
 					clrbuf(ibp);
 					if(error == VOP_BWRITE(ibp))
@@ -170,7 +170,7 @@ lfs_balloc(vp, offset, iosize, lbn, bpp)
 				brelse(bp);
 				return(ENOSPC);
 			} else {
-				DIP_SET(ip, blocks, DIP(ip, blocks) += bb);//ip->i_ffs1_blocks += bb;
+				DIP_SET(ip, blocks, DIP(ip, blocks) + bb);//ip->i_ffs1_blocks += bb;
 				ip->i_lfs->lfs_bfree -= bb;
 				if (iosize != fs->lfs_bsize)
 					clrbuf(bp);
@@ -222,7 +222,7 @@ lfs_fragextend(vp, osize, nsize, lbn, bpp)
 		return (error);
 	}
 #endif
-	DIP_SET(ip, blocks, DIP(ip, blocks) += bb);//ip->i_ffs1_blocks += bb;
+	DIP_SET(ip, blocks, DIP(ip, blocks) + bb);//ip->i_ffs1_blocks += bb;
 	ip->i_flag |= IN_CHANGE | IN_UPDATE;
 	fs->lfs_bfree -= fragstodb(fs, numfrags(fs, (nsize - osize)));
 	allocbuf(*bpp, nsize);
