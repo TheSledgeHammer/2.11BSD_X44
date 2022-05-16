2c34280f135ba66bcb3117f402c6f30bbbaadce3
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2022-05-16 15:55:05 +1000

    mpx reader/writer changes
    - Changes the mpx reader/writer to be more like the pipes from FreeBSD.

--- sys/devel/mpx/mpx.c
+++ sys/devel/mpx/mpx.c
@@ -25,6 +25,27 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*-
+ * SPDX-License-Identifier: BSD-4-Clause
+ *
+ * Copyright (c) 1996 John S. Dyson
+ * Copyright (c) 2012 Giovanni Trematerra
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice immediately at the beginning of the file, without modification,
+ *    this list of conditions, and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Absolutely no warranty of function or purpose is made by the author
+ *    John S. Dyson.
+ * 4. Modifications may be freely made to this file if the above conditions
+ *    are met.
+ */
 
 #include <sys/cdefs.h>
 #include <sys/systm.h>
@@ -54,6 +75,18 @@ struct fileops mpxops = {
 struct grouplist   	mpx_groups[NGROUPS];
 struct channellist  mpx_channels[NCHANS];
 
+static long amountmpxkva;
+static int mpxfragretry;
+static int mpxallocfail;
+static int mpxresizefail;
+static int mpxresizeallowed = 1;
+static long mpx_mindirect = MPX_MINDIRECT;
+
+static int mpx_paircreate(struct proc *, struct mpxpair **);
+static int mpxspace_new(struct mpx *, int);
+static int mpxspace(struct mpx *, int);
+static int mpx_create(struct mpx *, bool_t);
+
 void
 mpx_init(void)
 {
@@ -61,9 +94,8 @@ mpx_init(void)
 	register int 	i, j;
 
 	mpx = (struct mpx *)malloc(sizeof(struct mpx *), M_MPX, M_WAITOK);
-	mpxpair(mpx) = (union mpxpair *)malloc(sizeof(union mpxpair *), M_MPX, M_WAITOK);
 
-	MPX_LOCK_INIT(mpxpair(mpx));
+	MPX_LOCK_INIT(mpx->mpx_pair);
 
     for(i = 0; i <= NGROUPS; i++) {
     	LIST_INIT(&mpx_groups[i]);
@@ -74,146 +106,145 @@ mpx_init(void)
 	}
 }
 
-int
-mpx_count_groups(gp)
-	struct mpx_group 	*gp;
+static int
+mpx_paircreate(p, p_mpp)
+	struct proc 	*p;
+	struct mpxpair 	**p_mpp;
 {
-	int i, count;
-	count = 0;
-	for(i = 0; i < NGROUPS; i++) {
-		gp = mpx_get_group(i);
-		if(gp->mpg_index == i && gp != NULL) {
-			count++;
+	struct mpxpair *mpp;
+	struct mpx *wmpx, *rmpx;
+	int error;
+
+	*p_mpp = mpp = (struct mpxpair *)malloc(sizeof(struct mpxpair *), M_MPX, M_WAITOK);
+
+	rmpx = &mpp->mpp_rmpx;
+	wmpx = &mpp->mpp_wmpx;
+
+	error = mpx_create(rmpx, TRUE);
+	if (error != 0) {
+		goto fail;
+	}
+
+	error = mpx_create(wmpx, FALSE);
+	if (error != 0) {
+		goto fail;
+	}
+
+	rmpx->mpx_state |= MPX_DIRECTOK;
+	wmpx->mpx_state |= MPX_DIRECTOK;
+	return (0);
+
+fail:
+
+	free(mpp, M_MPX);
+	return (error);
+}
+
+static int
+mpxspace_new(cmpx, size)
+	struct mpx *cmpx;
+	int 		size;
+{
+	caddr_t buffer;
+	int error, cnt, firstseg;
+	static int curfail = 0;
+	static struct timeval lastfail;
+
+retry:
+	cnt = cmpx->mpx_buffer.cnt;
+	if (cnt > size) {
+		size = cnt;
+	}
+
+	size = round_page(size);
+	buffer = (caddr_t) vm_map_min(mpx_map);
+
+	error = vm_map_find(mpx_map, NULL, 0, (vm_offset_t *)&buffer, size, 0, FALSE);
+	if (error != KERN_SUCCESS) {
+		if (cmpx->mpx_buffer.buffer == NULL && size > SMALL_MPX_SIZE) {
+			size = SMALL_MPX_SIZE;
+			mpxfragretry++;
+			goto retry;
+		}
+		if (cmpx->mpx_buffer.buffer == NULL) {
+			mpxallocfail++;
+		} else {
+			mpxresizefail++;
+		}
+		return (ENOMEM);
+	}
+
+	/* copy data, then free old resources if we're resizing */
+	if (cnt > 0) {
+		if (cmpx->mpx_buffer.in <= cmpx->mpx_buffer.out) {
+			firstseg = cmpx->mpx_buffer.size - cmpx->mpx_buffer.out;
+			bcopy(&cmpx->mpx_buffer.buffer[cmpx->mpx_buffer.out], buffer, firstseg);
+			if ((cnt - firstseg) > 0) {
+				bcopy(cmpx->mpx_buffer.buffer, &buffer[firstseg],
+						cmpx->mpx_buffer.in);
+			}
+		} else {
+			bcopy(&cmpx->mpx_buffer.buffer[cmpx->mpx_buffer.out], buffer, cnt);
 		}
 	}
-	return (count);
-}
 
-int
-mpx_count_channels(cp)
-	struct mpx_channel 	*cp;
-{
-	int i, count;
-	count = 0;
-	for(i = 0; i < NCHANS; i++) {
-		cp = mpx_get_channel(i);
-		if(cp->mpc_index == i && cp != NULL) {
-			count++;
-		}
-	}
-	return (count);
-}
+	cmpx->mpx_buffer.buffer = buffer;
+	cmpx->mpx_buffer.size = size;
+	cmpx->mpx_buffer.in = cnt;
+	cmpx->mpx_buffer.out = 0;
+	cmpx->mpx_buffer.cnt = cnt;
 
-int
-mpx_compare_groups(gp1, gp2)
-	struct mpx_group *gp1, *gp2;
-{
-	if(gp1->mpg_index < gp2->mpg_index) {
-		return (-1);
-	} else if(gp1->mpg_index > gp2->mpg_index) {
-		return (1);
-	} else {
-		return (0);
-	}
-}
-
-int
-mpx_compare_channels(cp1, cp2)
-	struct mpx_channel 	*cp1, *cp2;
-{
-	if(cp1->mpc_index < cp2->mpc_index) {
-		return (-1);
-	} else if(cp1->mpc_index > cp2->mpc_index) {
-		return (1);
-	} else {
-		return (0);
-	}
-}
-
-struct mpx_writer *
-mpx_writer(mpx, state)
-	struct mpx *mpx;
-	int state;
-{
-	register struct mpx_writer 	*wmpx;
-
-	MPX_LOCK(mpxpair(mpx));
-	wmpx = mpxpair(mpx)->mpp_wmpx;
-	if(wmpx != NULL) {
-		wmpx->mpw_group = mpx->mpx_group;
-		wmpx->mpw_channel = mpx->mpx_channel;
-		wmpx->mpw_size = MPX_WRITER_SIZE;
-		wmpx->mpw_buffer = MPX_BUFFER(MPX_WRITER_SIZE);
-		wmpx->mpw_state = state;
-		MPX_UNLOCK(mpxpair(mpx));
-		return (wmpx);
-	}
-	MPX_UNLOCK(mpxpair(mpx));
-	return (NULL);
-}
-
-struct mpx_reader *
-mpx_reader(mpx, state)
-	struct mpx *mpx;
-	int state;
-{
-	register struct mpx_reader *rmpx;
-
-	MPX_LOCK(mpxpair(mpx));
-	rmpx = mpxpair(mpx)->mpp_rmpx;
-	if(rmpx != NULL) {
-		rmpx->mpr_group = mpx->mpx_group;
-		rmpx->mpr_channel = mpx->mpx_channel;
-		rmpx->mpr_size = MPX_READER_SIZE;
-		rmpx->mpr_buffer = MPX_BUFFER(MPX_READER_SIZE);
-		rmpx->mpr_state = state;
-		MPX_UNLOCK(mpxpair(mpx));
-		return (rmpx);
-	}
-	MPX_UNLOCK(mpxpair(mpx));
-	return (NULL);
-}
-
-int
-mpxread(mpx, state)
-	struct mpx 		*mpx;
-	int 			state;
-{
-	register struct mpx_reader 	*rmpx;
-
-	rmpx = mpx_reader(mpx, state);
-	if(rmpx == NULL) {
-		return (1);
-	}
 	return (0);
 }
 
-int
-mpxwrite(mpx, state)
-	struct mpx 		*mpx;
-	int 			state;
+static int
+mpxspace(cmpx, size)
+	struct mpx *cmpx;
+	int 		size;
 {
-	register struct mpx_writer 	*wmpx;
+    KASSERT(cmpx->mpx_state & MPX_LOCKFL);
+    return (mpxspace_new(cmpx, size));
+}
 
-	wmpx = mpx_writer(mpx, state);
-	if(wmpx == NULL) {
-		return (1);
-	}
-	return (0);
+static int
+mpx_create(mpx, large_backing)
+	struct mpx 	*mpx;
+	bool_t  	large_backing;
+{
+	int error;
+
+	error = mpxspace_new(mpx,
+			!large_backing || amountmpxkva > maxmpxkva / 2 ?
+					SMALL_MPX_SIZE : MPX_SIZE);
+
+	return (error);
+}
+
+struct mpx_group *
+mpx_allocate_groups(mpx, ngroups)
+	struct mpx 	*mpx;
+	int 		ngroups;
+{
+	register struct mpx_group *result;
+	result = (struct mpx_group *)calloc(ngroups, sizeof(struct mpx_group *), M_MPX, M_WAITOK);
+	mpx->mpx_group = result;
+
+	return (result);
 }
 
 void
-mpx_create_group(idx)
-    int idx;
+mpx_create_group(mpx, idx)
+	struct mpx 	*mpx;
+	int idx;
 {
-    struct grouplist *group;
-    struct mpx_group *gp;
+	struct grouplist *group;
+	struct mpx_group *gp;
 
-    group = &mpx_groups[idx];
-    gp = (struct mpx_group *)calloc(NGROUPS, sizeof(struct mpx_group *), M_MPX, M_WAITOK);
+	group = &mpx_groups[idx];
+	gp = mpx_allocate_groups(mpx, NGROUPS);
     gp->mpg_index = idx;
 
-    LIST_INSERT_HEAD(group, gp, mpg_node);
+	LIST_INSERT_HEAD(group, gp, mpg_node);
 }
 
 struct mpx_group *
@@ -245,19 +276,32 @@ mpx_remove_group(gp, idx)
 	}
 }
 
+struct mpx_channel *
+mpx_allocate_channels(mpx, nchans)
+	struct mpx 	*mpx;
+	int 		nchans;
+{
+	register struct mpx_channel *result;
+	result = (struct mpx_channel *)calloc(nchans, sizeof(struct mpx_chan *), M_MPX, M_WAITOK);
+	mpx->mpx_channel = result;
+
+	return (result);
+}
+
 void
-mpx_create_channel(gp, idx, flags)
-	struct mpx_group *gp;
-    int idx, flags;
+mpx_create_channel(mpx, gp, idx, flags)
+	struct mpx 			*mpx;
+	struct mpx_group 	*gp;
+	int 				idx, flags;
 {
     struct channellist *chan;
     struct mpx_channel *cp;
 
     chan = &mpx_channels[idx];
-    cp = (struct mpx_channel *)calloc(NCHANS, sizeof(struct mpx_chan *), M_MPX, M_WAITOK);
+    cp = mpx_allocate_channels(mpx, NCHANS);
+    cp->mpc_group = gp;
     cp->mpc_index = idx;
     cp->mpc_flags = flags;
-    cp->mpc_group = gp;
 
     LIST_INSERT_HEAD(chan, cp, mpc_node);
 }
@@ -364,19 +408,24 @@ mpx_connect(cp1, cp2)
 	struct mpx_channel 	*cp1, *cp2;
 {
 	struct mpx_channel 	*cp3;
-	int i, cnt1, cnt2, total;
+	int i, cnt, total;
 
 	/* check channels used */
-	cnt1 = 0;
-	cnt2 = 0;
+	cnt = 0;
 	for(i = 0; i < NCHANS; i++) {
 		cp1 = mpx_get_channel(i);
 		cp2 = mpx_get_channel(i);
-		cnt1 = mpx_count_channels(cp1);
-		cnt2 = mpx_count_channels(cp2);
+		if (mpx_compare_channels(cp1, cp2) != 0) {
+			if((cp1->mpc_index == i && cp1 != NULL) || (cp2->mpc_index == i && cp2 != NULL)) {
+				cnt++;
+			}
+		} else {
+			cnt++;
+		}
 	}
+
 	/* merge channels */
-	total = (cnt1 + cnt2);
+	total = cnt;
 	if (total < NCHANS) {
 		cp3 = mpx_merge_channels(cp1, cp2);
 		if(cp3) {
@@ -437,28 +486,6 @@ mpx_join()
 mpx_leave()
 {
 
-}
-
-/*
- * creates mpxspace.
- * default: 10 mpx groups with 20 mpx channels per group
- */
-void
-mpxspace(flags)
-	int flags;
-{
-    register struct mpx_group 	*gp;
-    register int i, j;
-
-    for(i = 0; i <= NGROUPS; i++) {
-        mpx_create_group(i);
-        LIST_FOREACH(gp, &mpx_groups[i], mpg_node) {
-            for(j = 0; j <= NCHANS; j++) {
-                mpx_create_channel(gp, j, flags);
-                gp->mpg_channel = mpx_get_channel(j);
-            }
-        }
-    }
 }
 
 /*
@@ -541,4 +568,30 @@ mpx_kqfilter(fp, kn)
 	struct knote *kn;
 {
 	return (0);
+}
+
+int
+mpx_compare_groups(gp1, gp2)
+	struct mpx_group *gp1, *gp2;
+{
+	if(gp1->mpg_index < gp2->mpg_index) {
+		return (-1);
+	} else if(gp1->mpg_index > gp2->mpg_index) {
+		return (1);
+	} else {
+		return (0);
+	}
+}
+
+int
+mpx_compare_channels(cp1, cp2)
+	struct mpx_channel 	*cp1, *cp2;
+{
+	if(cp1->mpc_index < cp2->mpc_index) {
+		return (-1);
+	} else if(cp1->mpc_index > cp2->mpc_index) {
+		return (1);
+	} else {
+		return (0);
+	}
 }
--- sys/devel/mpx/mpx.h
+++ sys/devel/mpx/mpx.h
@@ -33,32 +33,59 @@
 #include <sys/queue.h>
 #include <sys/tree.h>
 
-#define	NINDEX		15 	/* comes from ufs inode */
-#define	NGROUPS		10	/* number of group structures */
-#define	NCHANS		20	/* number of channel structures */
-#define	NLEVELS		4
+#define	NINDEX				15 	/* comes from ufs inode */
+#define	NGROUPS				10	/* number of group structures */
+#define	NCHANS				20	/* number of channel structures */
+#define	NLEVELS				4
 
-/* mpx reader/buffer */
-struct mpx_reader {
-	struct mpx_group		*mpr_group;		/* group */
-    struct mpx_channel 		*mpr_channel;   /* channel */
-    int 				    mpr_state;      /* state */
-    size_t		            mpr_size;		/* size of buffer */
-    caddr_t		            mpr_buffer;		/* kva of buffer */
+extern struct vm_map 		mpx_map_store;
+#define	mpx_map				(&mpx_map_store)
+
+#ifndef MPX_SIZE
+#define MPX_SIZE			16384
+#endif
+
+#ifndef BIG_MPX_SIZE
+#define BIG_MPX_SIZE		(64*1024)
+#endif
+
+#ifndef SMALL_MPX_SIZE
+#define SMALL_MPX_SIZE		PAGE_SIZE
+#endif
+
+/*
+ * MPX_MINDIRECT MUST be smaller than MPX_SIZE and MUST be bigger
+ * than MPX_BUF.
+ */
+#ifndef MPX_MINDIRECT
+#define MPX_MINDIRECT		8192
+#endif
+
+#define MPXNPAGES			(BIG_MPX_SIZE / PAGE_SIZE + 1)
+
+//#define MPX_BUFFER			(round_page(NGROUPS + NCHANS + MPX_SIZE))
+
+struct mpxbuf {
+	u_int					cnt;		/* number of chars currently in buffer */
+	u_int					in;			/* in pointer */
+	u_int					out;		/* out pointer */
+	u_int					size;		/* size of buffer */
+	caddr_t					buffer;		/* kva of buffer */
 };
 
-/* mpx writer/buffer */
-struct mpx_writer {
-	struct mpx_group		*mpw_group;		/* group */
-    struct mpx_channel      *mpw_channel;  	/* channel */
-    int 				    mpw_state;      /* state */
-    size_t		            mpw_size;		/* size of buffer */
-    caddr_t		            mpw_buffer;		/* kva of buffer */
-};
-
-#define MPX_READER_SIZE		sizeof(struct mpx_reader *)
-#define MPX_WRITER_SIZE		sizeof(struct mpx_writer *)
-#define MPX_BUFFER(size)	(round_page(NGROUPS + NCHANS + (size)))
+/*
+ * Bits in mpx_state.
+ */
+#define MPX_ASYNC		0x004	/* Async? I/O. */
+#define MPX_WANTR		0x008	/* Reader wants some characters. */
+#define MPX_WANTW		0x010	/* Writer wants space to put characters. */
+#define MPX_WANT		0x020	/* Pipe is wanted to be run-down. */
+#define MPX_SEL			0x040	/* Pipe has a select active. */
+#define MPX_EOF			0x080	/* Pipe is in EOF condition. */
+#define MPX_LOCKFL		0x100	/* Process has exclusive access to pointers/data. */
+#define MPX_LWANT		0x200	/* Process wants exclusive access to pointers/data. */
+#define MPX_DIRECTW		0x400	/* Pipe direct write active. */
+#define MPX_DIRECTOK	0x800	/* Direct mode ok. */
 
 struct channellist;
 LIST_HEAD(channellist, mpx_channel);
@@ -77,22 +104,25 @@ struct mpx_group {
     int 					 mpg_index;    /* group index */
 };
 
-union mpxpair {
-    struct mpx_writer       mpp_wmpx;
-    struct mpx_reader       mpp_rmpx;
-    struct lock_object		mpp_lock;
-};
-
 struct mpx {
-	union  mpxpair			mpx_pair;
+	struct mpxbuf			mpx_buffer;
     struct mpx_group		*mpx_group;
     struct mpx_channel	    *mpx_channel;
+    struct mpxpair			*mpx_pair;
+	int 				    mpx_state;      /* state */
+	struct selinfo 			mpx_sel;		/* for compat with select */
+	struct timespec	 		mpx_atime;		/* time of last access */
+	struct timespec 		mpx_mtime;		/* time of last modify */
+	struct timespec 		mpx_ctime;		/* time of status change */
+
     struct file				*mpx_file;
 };
 
-#define mpxpair(mpx)		((mpx)->mpx_pair)
-
-
+struct mpxpair {
+	struct mpx				mpp_wmpx;
+	struct mpx				mpp_rmpx;
+    struct lock_object		mpp_lock;
+};
 
 #define MPX_LOCK_INIT(mpp)	simple_lock_init((mpp)->mpp_lock, "mpxpair_lock")
 #define MPX_LOCK(mpp)		simple_lock((mpp)->mpp_lock)
@@ -100,6 +130,7 @@ struct mpx {
 
 extern struct grouplist     mpx_groups[];
 extern struct channellist   mpx_channels[];
+extern long					maxmpxkva;
 
 void                		mpx_init(void);
 void                		mpx_create_group(int);
