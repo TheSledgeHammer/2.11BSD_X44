997bb92bd5f9a589572217b1935f8713947b0194
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2022-05-24 10:36:21 +1000

    Update db_command.c

--- sys/ddb/db_command.c
+++ sys/ddb/db_command.c
@@ -35,6 +35,7 @@
 #include <sys/systm.h>
 #include <sys/reboot.h>
 #include <sys/proc.h>
+#include <sys/sysdecl.h>
 
 #include <machine/setjmp.h>	
 #include <machine/db_machdep.h>		/* type definitions */
