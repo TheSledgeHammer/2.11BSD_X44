672b931a78e9fea2f34b9f611204758512b72dac
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2022-07-13 18:54:20 +1000

    icu.h updates

--- sys/arch/i386/isa/icu.h
+++ sys/arch/i386/isa/icu.h
@@ -48,38 +48,44 @@ extern unsigned short			imen;		/* interrupt mask enable */
 extern unsigned short			cpl;		/* current priority level mask */
 extern unsigned short			ipending;
 extern unsigned short			idepth;
+extern int 				imask[];
+extern int 				iunmask[];
+extern int 				intrtype[];
+extern int 				intrmask[];
+extern int 				intrlevel[];
 
-#define	INTREN(s)				imen &= ~(s)
-#define	INTRDIS(s)				imen |= (s)
+#define	INTREN(s)			imen &= ~(s)
+#define	INTRDIS(s)			imen |= (s)
 #define	INTRMASK(msk,s)			msk |= (s)
 #define INTRUNMASK(msk,s)		(msk &= ~(s))
+#define SOFTINTR_MASK(mask, sir)	((mask) = 1 << (sir))
 
 /*
  * Interrupt enable bits -- in order of priority
  */
-#define	IRQ0					0x0001		/* highest priority - timer */
-#define	IRQ1					0x0002
-#define	IRQ_SLAVE				0x0004
-#define	IRQ8					0x0100
-#define	IRQ9					0x0200
-#define	IRQ2					IRQ9
-#define	IRQ10					0x0400
-#define	IRQ11					0x0800
-#define	IRQ12					0x1000
-#define	IRQ13					0x2000
-#define	IRQ14					0x4000
-#define	IRQ15					0x8000
-#define	IRQ3					0x0008
-#define	IRQ4					0x0010
-#define	IRQ5					0x0020
-#define	IRQ6					0x0040
-#define	IRQ7					0x0080		/* lowest - parallel printer */
+#define	IRQ0				0x0001		/* highest priority - timer */
+#define	IRQ1				0x0002
+#define	IRQ_SLAVE			0x0004
+#define	IRQ8				0x0100
+#define	IRQ9				0x0200
+#define	IRQ2				IRQ9
+#define	IRQ10				0x0400
+#define	IRQ11				0x0800
+#define	IRQ12				0x1000
+#define	IRQ13				0x2000
+#define	IRQ14				0x4000
+#define	IRQ15				0x8000
+#define	IRQ3				0x0008
+#define	IRQ4				0x0010
+#define	IRQ5				0x0020
+#define	IRQ6				0x0040
+#define	IRQ7				0x0080		/* lowest - parallel printer */
 
 /*
  * Interrupt Control offset into Interrupt descriptor table (IDT)
  */
-#define	ICU_OFFSET				32			/* 0-31 are processor exceptions */
-#define	ICU_LEN					16			/* 32-47 are ISA interrupts */
+#define	ICU_OFFSET			32		/* 0-31 are processor exceptions */
+#define	ICU_LEN				16		/* 32-47 are ISA interrupts */
 
 #define	LEGAL_IRQ(x)			((x) >= 0 && (x) < ICU_LEN && (x) != 2)
 
