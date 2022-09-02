2662b9e777322b5c9e01b69f41cd1e6b85b784cb
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2022-09-02 14:33:06 +1000

    Update subr_hints.c

--- sys/kern/subr_hints.c
+++ sys/kern/subr_hints.c
@@ -42,8 +42,8 @@
  * Access functions for device resources.
  */
 /* Supplied by config(8) in ioconf.c */
-extern struct cfhint allhints[];	/* head of list of device hints */
-extern int cfhint_count; 			/* hint count */
+struct cfhint allhints[];	/* head of list of device hints */
+int cfhint_count; 			/* hint count */
 
 /* Runtime version */
 struct cfhint *hint = allhints;
