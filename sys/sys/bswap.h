83141af0e6a1163a58306f2ad5f41d87a6ffc456
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-01-12 16:45:56 +1100

    Update bswap.h

--- sys/sys/bswap.h
+++ sys/sys/bswap.h
@@ -15,13 +15,13 @@
 
 __BEGIN_DECLS
 #if defined(_KERNEL) || defined(_STANDALONE) || !defined(__BSWAP_RENAME)
-u_int16_t       bswap16 __P((u_int16_t));
-u_int32_t       bswap32 __P((u_int32_t));
+u_int16_t       bswap16(u_int16_t);
+u_int32_t       bswap32(u_int32_t);
 #else
-u_int16_t       bswap16 __P((u_int16_t)) __RENAME(__bswap16);
-u_int32_t       bswap32 __P((u_int32_t)) __RENAME(__bswap32);
+u_int16_t       bswap16(u_int16_t) __RENAME(__bswap16);
+u_int32_t       bswap32(u_int32_t) __RENAME(__bswap32);
 #endif
-u_int64_t       bswap64 __P((u_int64_t));
+u_int64_t       bswap64(u_int64_t);
 __END_DECLS
 #endif /* !_LOCORE */
 
