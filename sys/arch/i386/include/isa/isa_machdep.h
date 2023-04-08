70f7cdbd2337aea6befc4a2c1b4a2f8093e35c4e
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-04-08 11:57:37 +1000

    updated isa_machdep.h

--- sys/arch/i386/include/isa/isa_machdep.h
+++ sys/arch/i386/include/isa/isa_machdep.h
@@ -85,26 +85,77 @@
 
 #include <sys/queue.h>
 #include <machine/bus.h>
+#include <dev/core/isa/isadmavar.h>
 
 /*
  * Types provided to machine-independent ISA code.
  */
-typedef void *isa_chipset_tag_t;
+struct i386_isa_chipset {
+	struct isa_dma_state ic_dmastate;
+};
+typedef struct i386_isa_chipset *isa_chipset_tag_t;
 
-struct device;				/* XXX */
+struct device;		        /* XXX */
 struct isabus_attach_args;	/* XXX */
 
 /*
  * Functions provided to machine-independent ISA code.
  */
-int		isa_nmi(void);
+int	isa_nmi(void);
 void	isa_attach_hook(struct device *, struct device *, struct isabus_attach_args *);
 void	isa_detach_hook(isa_chipset_tag_t, struct device *);
-int		isa_intr_alloc(isa_chipset_tag_t, int, int, int *);
+int	isa_intr_alloc(isa_chipset_tag_t, int, int, int *);
 void	*isa_intr_establish(isa_chipset_tag_t, int, int, int, int (*)(void *), void *);
 void	isa_intr_disestablish(isa_chipset_tag_t, void *);
-int		isa_mem_alloc(bus_space_tag_t, bus_size_t, bus_size_t, bus_addr_t, int, bus_addr_t *, bus_space_handle_t *);
+int	isa_mem_alloc(bus_space_tag_t, bus_size_t, bus_size_t, bus_addr_t, int, bus_addr_t *, bus_space_handle_t *);
 void	isa_mem_free(bus_space_tag_t, bus_space_handle_t, bus_size_t);
+
+#define	isa_dmainit(ic, bst, dmat, d)								\
+	_isa_dmainit(&(ic)->ic_dmastate, (bst), (dmat), (d))
+#define	isa_dmacascade(ic, c)										\
+	_isa_dmacascade(&(ic)->ic_dmastate, (c))
+#define	isa_dmamaxsize(ic, c)										\
+	_isa_dmamaxsize(&(ic)->ic_dmastate, (c))
+#define	isa_dmamap_create(ic, c, s, f)								\
+	_isa_dmamap_create(&(ic)->ic_dmastate, (c), (s), (f))
+#define	isa_dmamap_destroy(ic, c)									\
+	_isa_dmamap_destroy(&(ic)->ic_dmastate, (c))
+#define	isa_dmastart(ic, c, a, n, p, f, bf)							\
+	_isa_dmastart(&(ic)->ic_dmastate, (c), (a), (n), (p), (f), (bf))
+#define	isa_dmaabort(ic, c)											\
+	_isa_dmaabort(&(ic)->ic_dmastate, (c))
+#define	isa_dmacount(ic, c)											\
+	_isa_dmacount(&(ic)->ic_dmastate, (c))
+#define	isa_dmafinished(ic, c)										\
+	_isa_dmafinished(&(ic)->ic_dmastate, (c))
+#define	isa_dmadone(ic, c)											\
+	_isa_dmadone(&(ic)->ic_dmastate, (c))
+#define	isa_dmafreeze(ic)											\
+	_isa_dmafreeze(&(ic)->ic_dmastate)
+#define	isa_dmathaw(ic)												\
+	_isa_dmathaw(&(ic)->ic_dmastate)
+#define	isa_dmamem_alloc(ic, c, s, ap, f)							\
+	_isa_dmamem_alloc(&(ic)->ic_dmastate, (c), (s), (ap), (f))
+#define	isa_dmamem_free(ic, c, a, s)								\
+	_isa_dmamem_free(&(ic)->ic_dmastate, (c), (a), (s))
+#define	isa_dmamem_map(ic, c, a, s, kp, f)							\
+	_isa_dmamem_map(&(ic)->ic_dmastate, (c), (a), (s), (kp), (f))
+#define	isa_dmamem_unmap(ic, c, k, s)								\
+	_isa_dmamem_unmap(&(ic)->ic_dmastate, (c), (k), (s))
+#define	isa_dmamem_mmap(ic, c, a, s, o, p, f)						\
+	_isa_dmamem_mmap(&(ic)->ic_dmastate, (c), (a), (s), (o), (p), (f))
+#define isa_drq_alloc(ic, c)										\
+	_isa_drq_alloc(&(ic)->ic_dmastate, c)
+#define isa_drq_free(ic, c)											\
+	_isa_drq_free(&(ic)->ic_dmastate, c)
+#define	isa_drq_isfree(ic, c)										\
+	_isa_drq_isfree(&(ic)->ic_dmastate, (c))
+#define	isa_malloc(ic, c, s, p, f)									\
+	_isa_malloc(&(ic)->ic_dmastate, (c), (s), (p), (f))
+#define	isa_free(a, p)												\
+	_isa_free((a), (p))
+#define	isa_mappage(m, o, p)										\
+	_isa_mappage((m), (o), (p))
 
 /*
  * ALL OF THE FOLLOWING ARE MACHINE-DEPENDENT, AND SHOULD NOT BE USED
