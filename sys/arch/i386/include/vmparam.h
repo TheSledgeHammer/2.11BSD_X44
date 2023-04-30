387e2f8a02b42f2631a8e0fa55cd86ee0bd3785d
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-04-30 13:56:55 +1000

    Update vmparam.h

--- sys/arch/i386/include/vmparam.h
+++ sys/arch/i386/include/vmparam.h
@@ -194,12 +194,12 @@
 #else
 #define VM_MIN_ADDRESS			    ((vm_offset_t)0)
 #endif
-#define	VM_MAXUSER_ADDRESS	    ((vm_offset_t)(PDIR_SLOT_PTE << L2_SHIFT))
+#define	VM_MAXUSER_ADDRESS      ((vm_offset_t)(PDIR_SLOT_PTE << L2_SHIFT))
 #define UPT_MIN_ADDRESS         ((vm_offset_t)((PDIR_SLOT_PTE << L2_SHIFT) + (PAGE_SIZE * 3))
 #define UPT_MAX_ADDRESS         ((vm_offset_t)((PDIR_SLOT_PTE << L2_SHIFT) + (PDIR_SLOT_PTE << L1_SHIFT)))
-#define VM_MAX_ADDRESS			    ((vm_offset_t)(UPT_MAX_ADDRESS))
-#define	VM_MIN_KERNEL_ADDRESS	  ((vm_offset_t)(PDIR_SLOT_KERN << L2_SHIFT))
-#define	VM_MAX_KERNEL_ADDRESS	  ((vm_offset_t)(PDIR_SLOT_APTE << L2_SHIFT))
+#define VM_MAX_ADDRESS          ((vm_offset_t)(UPT_MAX_ADDRESS))
+#define	VM_MIN_KERNEL_ADDRESS   ((vm_offset_t)(PDIR_SLOT_KERN << L2_SHIFT))
+#define	VM_MAX_KERNEL_ADDRESS   ((vm_offset_t)(PDIR_SLOT_APTE << L2_SHIFT))
 
 #define	PMAP_MAP_LOW			      (4 * 1024 * 1024)
 
