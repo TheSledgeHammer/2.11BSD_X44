a1affa34e0cb754a5ab589a5215ea96c917a0a93
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-05-15 01:51:23 +1000

    Update bsd.oscompat.mk

--- share/mk/bsd.oscompat.mk
+++ share/mk/bsd.oscompat.mk
@@ -1,5 +1,5 @@
 #	$211BSD: Makefile,v 1.0 2021/05/25 23:59:27 Exp $
-#  	OS Compatability (/contrib/external/compat)
+#  	OS Compatability (/contrib/compat)
 
 .if !defined(_BSD_OSCOMPAT_MK_)
 _BSD_OSCOMPAT_MK_=1
