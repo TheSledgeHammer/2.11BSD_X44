eb0b329cc424ff378285fc31eb254baacac8975b
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-05-15 20:05:03 +1000

    Update disklabel.c

--- lib/libc/gen/disklabel.c
+++ lib/libc/gen/disklabel.c
@@ -51,10 +51,8 @@ __RCSID("$NetBSD: disklabel.c,v 1.37 2012/06/25 22:32:43 abs Exp $");
 
 #if HAVE_NBTOOL_CONFIG_H
 #include <nbinclude/sys/disklabel.h>
-#include <nbinclude/disktab.h>
 #else
 #include <sys/disklabel.h>
-#include <disktab.h>
 #endif /* HAVE_NBTOOL_CONFIG_H */
 
 #include <assert.h>
