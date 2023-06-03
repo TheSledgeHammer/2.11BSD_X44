8d34f5c8e2db5f6f7230757126e0660567fa9dbd
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-06-03 12:24:07 +1000

    Update conf.c

--- sys/arch/i386/i386/conf.c
+++ sys/arch/i386/i386/conf.c
@@ -75,7 +75,7 @@
 #include "pty.h"
 #include "video.h"
 
-#include "evdev.h"
+//#include "evdev.h"
 #include "wsdisplay.h"
 #include "wskbd.h"
 #include "wsmouse.h"
@@ -258,5 +258,5 @@ wscons_init(devsw)
 	DEVSWIO_CONFIG_INIT(devsw, NWSMUX, NULL, &wsmux_cdevsw, NULL);			/* Wscons Multiplexor */
 	DEVSWIO_CONFIG_INIT(devsw, NWSFONT, NULL, &wsfont_cdevsw, NULL);		/* Wsfont */
 
-	DEVSWIO_CONFIG_INIT(devsw, NEVDEV, NULL, &evdev_cdevsw, NULL);			/* Evdev */
+	//DEVSWIO_CONFIG_INIT(devsw, NEVDEV, NULL, &evdev_cdevsw, NULL);			/* Evdev */
 }
