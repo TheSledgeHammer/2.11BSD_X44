cfca5e29fad5adb21ec77e973cc69ac4477d2898
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-08-15 21:40:20 +1000

    iso.h

--- sys/fs/isofs/cd9660/iso.h
+++ sys/fs/isofs/cd9660/iso.h
@@ -41,6 +41,10 @@
 #ifndef _ISOFS_CD9660_ISO_H_
 #define _ISOFS_CD9660_ISO_H_
 
+#include <sys/mount.h>
+
+//struct netexport;
+
 #define ISODCL(from, to) (to - from + 1)
 
 struct iso_volume_descriptor {
@@ -94,6 +98,42 @@ struct iso_primary_descriptor {
 };
 #define ISO_DEFAULT_BLOCK_SIZE		2048
 
+struct iso_supplementary_descriptor {
+	char type			        [ISODCL (  1,	1)]; /* 711 */
+	char id				        [ISODCL (  2,	6)];
+	char version			    [ISODCL (  7,	7)]; /* 711 */
+	char flags			        [ISODCL (  8,	8)]; /* 711? */
+	char system_id			    [ISODCL (  9,  40)]; /* achars */
+	char volume_id			    [ISODCL ( 41,  72)]; /* dchars */
+	char unused2			    [ISODCL ( 73,  80)];
+	char volume_space_size		[ISODCL ( 81,  88)]; /* 733 */
+	char escape			        [ISODCL ( 89, 120)];
+	char volume_set_size		[ISODCL (121, 124)]; /* 723 */
+	char volume_sequence_number	[ISODCL (125, 128)]; /* 723 */
+	char logical_block_size		[ISODCL (129, 132)]; /* 723 */
+	char path_table_size		[ISODCL (133, 140)]; /* 733 */
+	char type_l_path_table		[ISODCL (141, 144)]; /* 731 */
+	char opt_type_l_path_table	[ISODCL (145, 148)]; /* 731 */
+	char type_m_path_table		[ISODCL (149, 152)]; /* 732 */
+	char opt_type_m_path_table	[ISODCL (153, 156)]; /* 732 */
+	char root_directory_record	[ISODCL (157, 190)]; /* 9.1 */
+	char volume_set_id		    [ISODCL (191, 318)]; /* dchars */
+	char publisher_id		    [ISODCL (319, 446)]; /* achars */
+	char preparer_id		    [ISODCL (447, 574)]; /* achars */
+	char application_id		    [ISODCL (575, 702)]; /* achars */
+	char copyright_file_id		[ISODCL (703, 739)]; /* 7.5 dchars */
+	char abstract_file_id		[ISODCL (740, 776)]; /* 7.5 dchars */
+	char bibliographic_file_id	[ISODCL (777, 813)]; /* 7.5 dchars */
+	char creation_date		    [ISODCL (814, 830)]; /* 8.4.26.1 */
+	char modification_date		[ISODCL (831, 847)]; /* 8.4.26.1 */
+	char expiration_date		[ISODCL (848, 864)]; /* 8.4.26.1 */
+	char effective_date		    [ISODCL (865, 881)]; /* 8.4.26.1 */
+	char file_structure_version	[ISODCL (882, 882)]; /* 711 */
+	char unused4			    [ISODCL (883, 883)];
+	char application_data		[ISODCL (884, 1395)];
+	char unused5			    [ISODCL (1396, 2048)];
+};
+
 struct iso_directory_record {
 	char length					[ISODCL (1, 1)]; /* 711 */
 	char ext_attr_length		[ISODCL (2, 2)]; /* 711 */
@@ -112,22 +152,22 @@ struct iso_directory_record {
 #define ISO_DIRECTORY_RECORD_SIZE	33
 
 struct iso_extended_attributes {
-	u_char owner			[ISODCL (1, 4)]; /* 723 */
-	u_char group			[ISODCL (5, 8)]; /* 723 */
-	u_char perm				[ISODCL (9, 10)]; /* 9.5.3 */
-	char ctime				[ISODCL (11, 27)]; /* 8.4.26.1 */
-	char mtime				[ISODCL (28, 44)]; /* 8.4.26.1 */
-	char xtime				[ISODCL (45, 61)]; /* 8.4.26.1 */
-	char ftime				[ISODCL (62, 78)]; /* 8.4.26.1 */
-	char recfmt				[ISODCL (79, 79)]; /* 711 */
-	char recattr			[ISODCL (80, 80)]; /* 711 */
-	u_char reclen			[ISODCL (81, 84)]; /* 723 */
-	char system_id			[ISODCL (85, 116)]; /* achars */
-	char system_use			[ISODCL (117, 180)];
-	char version			[ISODCL (181, 181)]; /* 711 */
-	char len_esc			[ISODCL (182, 182)]; /* 711 */
-	char reserved			[ISODCL (183, 246)];
-	u_char len_au			[ISODCL (247, 250)]; /* 723 */
+	u_char owner			    [ISODCL (1, 4)]; /* 723 */
+	u_char group			    [ISODCL (5, 8)]; /* 723 */
+	u_char perm				    [ISODCL (9, 10)]; /* 9.5.3 */
+	char ctime				    [ISODCL (11, 27)]; /* 8.4.26.1 */
+	char mtime				    [ISODCL (28, 44)]; /* 8.4.26.1 */
+	char xtime				    [ISODCL (45, 61)]; /* 8.4.26.1 */
+	char ftime				    [ISODCL (62, 78)]; /* 8.4.26.1 */
+	char recfmt				    [ISODCL (79, 79)]; /* 711 */
+	char recattr			    [ISODCL (80, 80)]; /* 711 */
+	u_char reclen			    [ISODCL (81, 84)]; /* 723 */
+	char system_id			    [ISODCL (85, 116)]; /* achars */
+	char system_use			    [ISODCL (117, 180)];
+	char version			    [ISODCL (181, 181)]; /* 711 */
+	char len_esc			    [ISODCL (182, 182)]; /* 711 */
+	char reserved			    [ISODCL (183, 246)];
+	u_char len_au			    [ISODCL (247, 250)]; /* 723 */
 };
 
 /* CD-ROM Format type */
@@ -136,14 +176,6 @@ enum ISO_FTYPE  { ISO_FTYPE_DEFAULT, ISO_FTYPE_9660, ISO_FTYPE_RRIP, ISO_FTYPE_E
 #ifndef	ISOFSMNT_ROOT
 #define	ISOFSMNT_ROOT	0
 #endif
-
-struct fid;
-struct mbuf;
-struct nameidata;
-struct proc;
-struct statfs;
-struct ucred;
-struct vfsconf;
 
 struct iso_mnt {
 	int im_flags;
@@ -175,6 +207,14 @@ struct iso_mnt {
 #define lblktosize(imp, blk)	((blk) << (imp)->im_bshift)
 #define lblkno(imp, loc)		((loc) >> (imp)->im_bshift)
 #define blksize(imp, ip, lbn)	((imp)->logical_block_size)
+
+struct fid;
+struct mbuf;
+struct nameidata;
+struct proc;
+struct statfs;
+struct ucred;
+struct vfsconf;
 
 int cd9660_mount(struct mount *, char *, caddr_t, struct nameidata *, struct proc *);
 int cd9660_start(struct mount *, int, struct proc *);
