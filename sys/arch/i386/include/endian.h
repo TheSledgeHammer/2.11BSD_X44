fa284de1e002af578c035026c0bfacfd586f2543
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-11-07 13:49:58 +1100

    Update endian.h

--- sys/arch/i386/include/endian.h
+++ sys/arch/i386/include/endian.h
@@ -43,12 +43,7 @@
 #ifdef __GNUC__
 
 #include <machine/byte_swap.h>
-/*
-#define	ntohl(x)	((uint32_t)__byte_swap_long((uint32_t)(x)))
-#define	ntohs(x)	((uint16_t)__byte_swap_word((uint16_t)(x)))
-#define	htonl(x)	((uint32_t)__byte_swap_long((uint32_t)(x)))
-#define	htons(x)	((uint16_t)__byte_swap_word((uint16_t)(x)))
-*/
+
 #endif
 
 #endif /* !_I386_ENDIAN_H_ */
