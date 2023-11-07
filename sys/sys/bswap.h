b54304c2540ecf5479ce3efc3f2b4a96f8c896be
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2023-11-07 13:45:46 +1100

    Update bswap.h

--- sys/sys/bswap.h
+++ sys/sys/bswap.h
@@ -2,10 +2,6 @@
 
 /* Written by Manuel Bouyer. Public domain */
 
-#ifndef _MACHINE_BSWAP_H_	/* _BEFORE_ #ifndef _SYS_BSWAP_H_ */
-#include <machine/bswap.h>
-#endif
-
 #ifndef _SYS_BSWAP_H_
 #define _SYS_BSWAP_H_
 
@@ -13,15 +9,17 @@
 #include <sys/cdefs.h>
 #include <sys/types.h>
 
+#include <machine/bswap.h>
+
 __BEGIN_DECLS
 #if defined(_KERNEL) || defined(_STANDALONE) || !defined(__BSWAP_RENAME)
-u_int16_t       bswap16(u_int16_t);
-u_int32_t       bswap32(u_int32_t);
+u_int16_t       bswap16(u_int16_t) __attribute__((__const__));
+u_int32_t       bswap32(u_int32_t) __attribute__((__const__));
 #else
-u_int16_t       bswap16(u_int16_t) __RENAME(__bswap16);
-u_int32_t       bswap32(u_int32_t) __RENAME(__bswap32);
+u_int16_t       bswap16(u_int16_t) __RENAME(__bswap16) __attribute__((__const__));
+u_int32_t       bswap32(u_int32_t) __RENAME(__bswap32) __attribute__((__const__));
 #endif
-u_int64_t       bswap64(u_int64_t);
+u_int64_t       bswap64(u_int64_t) __attribute__((__const__));
 __END_DECLS
 #endif /* !_LOCORE */
 
