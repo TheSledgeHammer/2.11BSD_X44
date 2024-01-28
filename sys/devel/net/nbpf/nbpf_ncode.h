f040ffda00fe93699e1024a8ec03543ca64127dd
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-01-29 00:33:24 +1100

    Update nbpf_ncode.h

--- sys/devel/net/nbpf/nbpf_ncode.h
+++ sys/devel/net/nbpf/nbpf_ncode.h
@@ -48,7 +48,7 @@ struct nbpf_insn {
 	size_t 				sz;
 };
 
-int nbpf_filter(struct nbpf_insn *, struct mbuf *, int);
+int nbpf_filter(struct nbpf_insn *, nbpf_buf_t *, int);
 int nbpf_validate(struct nbpf_insn *, size_t);
 
 /* Error codes. */
