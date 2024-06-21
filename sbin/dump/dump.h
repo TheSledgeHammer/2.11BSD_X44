9e1b3533fed7772fb94eba02e3675955ffc99534
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-06-21 16:00:43 +1000

    added dump to sbin

---
+++ sbin/dump/dump.h
@@ -0,0 +1,224 @@
+/*-
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)dump.h	8.2 (Berkeley) 4/28/95
+ */
+
+union dinode {
+	struct ufs1_dinode dp1;
+	struct ufs2_dinode dp2;
+};
+
+#define DIP(dp, field) \
+	(is_ufs2 ? (dp)->dp2.di_##field : (dp)->dp1.di_##field)
+
+#define DIP_SET(dp, field, val) do {	\
+	if (is_ufs2)						\
+		(dp)->dp2.di_##field = (val);	\
+	else								\
+		(dp)->dp1.di_##field = (val);	\
+} while (0)
+
+/*
+ * Dump maps used to describe what is to be dumped.
+ */
+int	mapsize;	/* size of the state maps */
+char	*usedinomap;	/* map of allocated inodes */
+char	*dumpdirmap;	/* map of directories to be dumped */
+char	*dumpinomap;	/* map of files to be dumped */
+/*
+ * Map manipulation macros.
+ */
+#define	SETINO(ino, map) \
+	map[(u_int)((ino) - 1) / NBBY] |=  1 << ((u_int)((ino) - 1) % NBBY)
+#define	CLRINO(ino, map) \
+	map[(u_int)((ino) - 1) / NBBY] &=  ~(1 << ((u_int)((ino) - 1) % NBBY))
+#define	TSTINO(ino, map) \
+	(map[(u_int)((ino) - 1) / NBBY] &  (1 << ((u_int)((ino) - 1) % NBBY)))
+
+/*
+ *	All calculations done in 0.1" units!
+ */
+char	*disk;		/* name of the disk file */
+char	*tape;		/* name of the tape file */
+char	*dumpdates;	/* name of the file containing dump date information*/
+char	*temp;		/* name of the file for doing rewrite of dumpdates */
+char	lastlevel;	/* dump level of previous dump */
+char	level;		/* dump level of this dump */
+int		uflag;		/* update flag */
+int		diskfd;		/* disk file descriptor */
+int		tapefd;		/* tape file descriptor */
+int		pipeout;	/* true => output to standard output */
+ino_t	curino;		/* current inumber; used globally */
+int		newtape;	/* new tape flag */
+int		density;	/* density in 0.1" units */
+long	tapesize;	/* estimated tape size, blocks */
+long	tsize;		/* tape size in 0.1" units */
+long	asize;		/* number of 0.1" units written on current tape */
+int		etapes;		/* estimated number of tapes */
+int		nonodump;	/* if set, do not honor UF_NODUMP user flags */
+
+int		notify;		/* notify operator flag */
+int		blockswritten;	/* number of blocks written on current tape */
+int		tapeno;		/* current tape number */
+time_t	tstart_writing;	/* when started writing the first tape block */
+struct	fs *sblock;	/* the file system super block */
+char	sblock_buf[MAXBSIZE];
+long	dev_bsize;	/* block size of underlying disk device */
+int		dev_bshift;	/* log2(dev_bsize) */
+int		tp_bshift;	/* log2(TP_BSIZE) */
+
+/* operator interface functions */
+void	broadcast(char *message);
+void	lastdump(int arg);	/* int should be char */
+void	msg(const char *fmt, ...);
+void	msgtail(const char *fmt, ...);
+int		query(char *question);
+void	quit(const char *fmt, ...);
+void	set_operators(void);
+void	timeest(void);
+time_t	unctime(char *str);
+
+/* mapping rouintes */
+struct	dinode;
+long	blockest(struct dinode *dp);
+int		mapfiles(ino_t maxino, long *tapesize);
+int		mapdirs(ino_t maxino, long *tapesize);
+
+/* file dumping routines */
+void	blksout(ufs2_daddr_t *blkp, int frags, ino_t ino);
+void	bread(ufs2_daddr_t blkno, char *buf, int size);
+void	dumpino(struct dinode *dp, ino_t ino);
+void	dumpmap(char *map, int type, ino_t ino);
+void	writeheader(ino_t ino);
+
+/* tape writing routines */
+int	alloctape(void);
+void	close_rewind(void);
+void	dumpblock(ufs2_daddr_t blkno, int size);
+void	startnewtape(int top);
+void	trewind(void);
+void	writerec(char *dp, int isspcl);
+
+__dead void Exit(int status);
+void	dumpabort(int signo);
+void	getfstab(void);
+
+char	*rawname(char *cp);
+union	dinode *getino(ino_t inum, int *);
+
+/* rdump routines */
+#ifdef RDUMP
+void	rmtclose(void);
+int	rmthost(char *host);
+int	rmtopen(char *tape, int mode);
+int	rmtwrite(char *buf, int count);
+#endif /* RDUMP */
+
+void	interrupt(int signo);	/* in case operator bangs on console */
+
+/*
+ *	Exit status codes
+ */
+#define	X_FINOK		0	/* normal exit */
+#define	X_REWRITE	2	/* restart writing from the check point */
+#define	X_ABORT		3	/* abort dump; don't attempt checkpointing */
+
+#define	OPGRENT	"operator"		/* group entry to notify */
+#define DIALUP	"ttyd"			/* prefix for dialups */
+
+struct	fstab *fstabsearch(char *key);	/* search fs_file and fs_spec */
+
+#ifndef NAME_MAX
+#define NAME_MAX 255
+#endif
+
+/*
+ *	The contents of the file _PATH_DUMPDATES is maintained both on
+ *	a linked list, and then (eventually) arrayified.
+ */
+struct dumpdates {
+	char	dd_name[NAME_MAX+3];
+	char	dd_level;
+	time_t	dd_ddate;
+};
+
+struct dthead;
+SLIST_HEAD(dthead, dumptime);
+struct dumptime {
+	struct	dumpdates dt_value;
+	SLIST_ENTRY(duptime) dt_list;
+};
+extern struct dthead dthead; /* head of the list version */
+int	nddates;		/* number of records (might be zero) */
+int	ddates_in;		/* we have read the increment file */
+struct	dumpdates **ddatev;	/* the arrayfied version */
+void	initdumptimes(void);
+void	getdumptime(void);
+void	putdumptime(void);
+#define	ITITERATE(i, ddp) \
+	for (ddp = ddatev[i = 0]; i < nddates; ddp = ddatev[++i])
+
+void	sig(int signo);
+
+/*
+ * Compatibility with old systems.
+ */
+#ifdef COMPAT
+#include <sys/file.h>
+#define	strchr(a,b)	index(a,b)
+#define	strrchr(a,b)	rindex(a,b)
+extern char *strdup(), *ctime();
+extern int read(), write();
+extern int errno;
+#endif
+
+#ifndef	_PATH_UTMP
+#define	_PATH_UTMP	"/etc/utmp"
+#endif
+#ifndef	_PATH_FSTAB
+#define	_PATH_FSTAB	"/etc/fstab"
+#endif
+
+#ifdef sunos
+extern char *calloc();
+extern char *malloc();
+extern long atol();
+extern char *strcpy();
+extern char *strncpy();
+extern char *strcat();
+extern time_t time();
+extern void endgrent();
+extern __dead void exit();
+extern off_t lseek();
+extern const char *strerror();
+#endif
