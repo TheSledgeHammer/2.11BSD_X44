711f470e292ffbb1a6cc62114baa8374c79eb40a
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-07-26 10:53:09 +1000

    Update mars.h

--- sys/crypto/mars/mars.h
+++ sys/crypto/mars/mars.h
@@ -5,7 +5,7 @@ typedef struct {
 	u_int32_t l_key[40];
 } mars_ctx;
 
-void mars_set_key(mars_ctx *ctx, const u_int8_t in_key[], const u_int8_t key_len);
+void mars_set_key(mars_ctx *ctx, const u_int8_t in_key[], u_int8_t key_len);
 void mars_encrypt(mars_ctx *ctx, const u_int8_t in_blk[], u_int8_t out_blk[]);
 void mars_decrypt(mars_ctx *ctx, const u_int8_t in_blk[], u_int8_t out_blk[]);
 
