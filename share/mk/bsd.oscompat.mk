8d84df8093f32a792b8d73056214b3074b6f44f7
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-08-16 11:24:52 +1000

    Update bsd.oscompat.mk

--- share/mk/bsd.oscompat.mk
+++ share/mk/bsd.oscompat.mk
@@ -6,7 +6,13 @@ _BSD_OSCOMPAT_MK_=1
 
 .include <bsd.own.mk>
 
-MKPLAN9= yes
+MK211BSD= 	no
+MKDRAGONFLYBSD=	no
+MKFREEBSD=	no
+MKNETBSD=	no
+MKOPENBSD=	no
+MKPLAN9= 	no
+MKSOLARIS=	no	
 
 .include <bsd.sys.mk>
 
