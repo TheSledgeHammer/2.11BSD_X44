a3f15f57ef1085184bdcfbbea23cb9330b12a19f
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-09-19 11:36:26 +1000

    Update encoding.h

--- include/encoding.h
+++ include/encoding.h
@@ -39,45 +39,45 @@ typedef uint32_t 	_csid_t;
 
 /* generic encoding structures */
 
-typedef struct {
-	u_char				type;
+typedef struct _Encoding_Charset {
+	unsigned char		type;
 #define	CS94			(0U)
 #define	CS96			(1U)
 #define	CS94MULTI		(2U)
 #define	CS96MULTI		(3U)
-	u_char				final;
-	u_char				interm;
-	u_char				vers;
+	unsigned char		final;
+	unsigned char		interm;
+	unsigned char		vers;
 } _Encoding_Charset;
 
-typedef struct {
-	wchar_t				*ch;
-	int 				chlen;
+typedef struct _Encoding_State {
+	wchar_t			*ch;
+	int 			chlen;
 	/* UTF16, UTF32 & UES extension */
-	int					current_endian;
+	int			current_endian;
 	/* ISO2022 extension */
 	_Encoding_Charset	g[4];
-	int					gl:3;
-	int					gr:3;
-	int					singlegl:3;
-	int					singlegr:3;
-	int 				flags;
+	int			gl:3;
+	int			gr:3;
+	int			singlegl:3;
+	int			singlegr:3;
+	int 			flags;
 } _Encoding_State;
 
-typedef struct {
-	size_t				state_size;
-	size_t				mb_cur_max;
+typedef struct _Encoding_Traits {
+	size_t			state_size;
+	size_t			mb_cur_max;
 } _Encoding_Traits;
 
-typedef struct {
-	unsigned int			count[4];
-	wchar_t				bits[4];
-	wchar_t				mask;
+typedef struct _Encoding_Info {
+	unsigned int		count[4];
+	wchar_t			bits[4];
+	wchar_t			mask;
 	unsigned int		mb_cur_max;
 	_Encoding_Traits	*traits;
 	/* UTF16, UTF32 & UES extension */
-	int					preffered_endian;
-	uint32_t			mode;
+	int			preffered_endian;
+	uint32_t		mode;
 #define _ENDIAN_UNKNOWN	0
 #define _ENDIAN_BIG		1
 #define _ENDIAN_LITTLE	2
@@ -85,9 +85,9 @@ typedef struct {
 	/* ISO2022 extension */
 	_Encoding_Charset	*recommend[4];
 	_Encoding_Charset	initg[4];
-	size_t				recommendsize[4];
-	int					maxcharset;
-	int					flags;
+	size_t			recommendsize[4];
+	int			maxcharset;
+	int			flags;
 #define	F_8BIT			0x0001
 #define	F_NOOLD			0x0002
 #define	F_SI			0x0010	/*0F*/
@@ -105,7 +105,7 @@ typedef struct {
 #define	F_SS3R			0x4000	/*8F*/
 } _Encoding_Info;
 
-typedef struct {
+typedef struct _Encoding_TypeInfo {
 	_Encoding_Info 		ei;
 	struct {
 		_Encoding_State	s_mblen;
@@ -121,26 +121,26 @@ typedef struct {
 	} states;
 } _Encoding_TypeInfo;
 
-#define _MODE_UTF32							0x00000001U
-#define _MODE_FORCE_ENDIAN					0x00000002U
+#define _MODE_UTF32				0x00000001U
+#define _MODE_FORCE_ENDIAN			0x00000002U
 
 /*
  * macros
  */
-#define _CEI_TO_EI(_cei_)					(&(_cei_)->ei)
+#define _CEI_TO_EI(_cei_)			(&(_cei_)->ei)
 #define _CEI_TO_STATE(_cei_, _func_)		(_cei_)->states.s_##_func_
-#define _TO_CEI(_cl_)						((_CTYPE_INFO*)(_cl_))
-#define _TO_EI(_cl_)						((_ENCODING_INFO*)(_cl_))
-#define _TO_STATE(_ps_)						((_ENCODING_STATE*)(_ps_))
+#define _TO_CEI(_cl_)				((_CTYPE_INFO*)(_cl_))
+#define _TO_EI(_cl_)				((_ENCODING_INFO*)(_cl_))
+#define _TO_STATE(_ps_)				((_ENCODING_STATE*)(_ps_))
 
-#define _ENCODING_INFO						_Encoding_Info
-#define _CTYPE_INFO							_Encoding_TypeInfo
-#define _ENCODING_CHARSET                   			_Encoding_Charset
-#define _ENCODING_STATE						_Encoding_State
-#define _ENCODING_TRAITS					_Encoding_Traits
-#define _ENCODING_MB_CUR_MAX(_ei_)			(_ei_)->mb_cur_max
+#define _ENCODING_INFO				_Encoding_Info
+#define _CTYPE_INFO				_Encoding_TypeInfo
+#define _ENCODING_CHARSET                   	_Encoding_Charset
+#define _ENCODING_STATE				_Encoding_State
+#define _ENCODING_TRAITS			_Encoding_Traits
+#define _ENCODING_MB_CUR_MAX(_ei_)		(_ei_)->mb_cur_max
 #define _ENCODING_IS_STATE_DEPENDENT		0
 #define _STATE_NEEDS_EXPLICIT_INIT(_ps_)	0
-#define _STATE_FLAG_INITIALIZED				0
+#define _STATE_FLAG_INITIALIZED			0
 
 #endif /* _ENCODING_H_ */
