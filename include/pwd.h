b9cfbe9c2f7c62314f64d4a1e392a4a3b2bb3a47
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-10-24 12:31:14 +1100

    Update pwd.h

--- include/pwd.h
+++ include/pwd.h
@@ -83,6 +83,6 @@ int		pw_gensalt(char *, size_t, const char *, const char *);
 int		pw_scan(char *, struct passwd *, int *);
 const char	*user_from_uid(uid_t, int);
 int		uid_from_user(const char *, uid_t *);
-int		pwcache_userdb(int (*)(int), void (*)(void), struct passwd * (*)(const char *), struct passwd * (*)(uid_t));
+int		pwcache_userdb(int (*)(int), void (*)(void), struct passwd * (*)(char *), struct passwd * (*)(uid_t));
 __END_DECLS
 #endif /* !_PWD_H_ */
