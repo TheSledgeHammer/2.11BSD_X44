a7c3de96a9e3c0428073f5fb385701ef8edb0f89
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-11-07 18:43:50 +1100

    ruserpass.c update

--- lib/libc/net/rexec.c
+++ lib/libc/net/rexec.c
@@ -56,7 +56,7 @@ retry:
 	sin.sin_family = hp->h_addrtype;
 	sin.sin_port = rport;
 	bcopy(hp->h_addr, (caddr_t)&sin.sin_addr, hp->h_length);
-	if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+	if (connect(s, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
 		if (errno == ECONNREFUSED && timo <= 16) {
 			(void) close(s);
 			sleep(timo);
@@ -72,31 +72,32 @@ retry:
 	} else {
 		char num[8];
 		int s2, sin2len;
-		
+
 		s2 = socket(AF_INET, SOCK_STREAM, 0);
 		if (s2 < 0) {
 			(void) close(s);
 			return (-1);
 		}
 		listen(s2, 1);
-		sin2len = sizeof (sin2);
-		if (getsockname(s2, (struct sockaddr *)&sin2, &sin2len) < 0 ||
-		  sin2len != sizeof (sin2)) {
+		sin2len = sizeof(sin2);
+		if (getsockname(s2, (struct sockaddr*) &sin2, &sin2len) < 0
+				|| sin2len != sizeof(sin2)) {
 			perror("getsockname");
 			(void) close(s2);
 			goto bad;
 		}
-		port = ntohs((u_short)sin2.sin_port);
+		port = ntohs((u_short) sin2.sin_port);
 		(void) sprintf(num, "%u", port);
-		(void) write(s, num, strlen(num)+1);
-		{ int len = sizeof(from);
-		  s3 = accept(s2, (struct sockaddr *)&from, &len);
-		  close(s2);
-		  if (s3 < 0) {
-			perror("accept");
-			port = 0;
-			goto bad;
-		  }
+		(void) write(s, num, strlen(num) + 1);
+		{
+			int len = sizeof(from);
+			s3 = accept(s2, (struct sockaddr*) &from, &len);
+			close(s2);
+			if (s3 < 0) {
+				perror("accept");
+				port = 0;
+				goto bad;
+			}
 		}
 		*fd2p = s3;
 	}
