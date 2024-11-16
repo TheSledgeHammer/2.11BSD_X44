b114cc3af39df2c083813ef4cab29ff1616bd1c2
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-11-16 21:43:42 +1100

    Update resolv.h

--- include/resolv.h
+++ include/resolv.h
@@ -39,7 +39,7 @@
 #define	RES_TIMEOUT			5		/* seconds between retries */
 #define	MAXRESOLVSORT			10		/* number of net to sort on */
 
-#define	RES_MAXTIME			65535	/* Infinity, in milliseconds. */
+#define	RES_MAXTIME			65535		/* Infinity, in milliseconds. */
 
 struct __res_state {
 	int				retrans;	 		/* retransmition time interval */
@@ -69,7 +69,7 @@ struct __res_state {
 		int			nssocks[MAXNS];
 		struct __res_state_ext	*ext;				/* extension for IPv6 */
 	} u;
-	u_char			*_rnd;				/* PRIVATE: random state */
+	u_char				*_rnd;				/* PRIVATE: random state */
 };
 
 union res_sockaddr_union {
@@ -100,12 +100,12 @@ typedef struct __res_state_ext 		*res_state_ext;
 /*%
  * Resolver flags (used to be discrete per-module statics ints).
  */
-#define	RES_F_VC			0x00000001	/* socket is TCP */
-#define	RES_F_CONN			0x00000002	/* socket is connected */
+#define	RES_F_VC		0x00000001	/* socket is TCP */
+#define	RES_F_CONN		0x00000002	/* socket is connected */
 #define	RES_F_EDNS0ERR		0x00000004	/* EDNS0 caused errors */
 #define	RES_F__UNUSED		0x00000008	/* (unused) */
 #define	RES_F_LASTMASK		0x000000F0	/* ordinal server of last res_nsend */
-#define	RES_F_LASTSHIFT		4			/* bit position of LASTMASK "flag" */
+#define	RES_F_LASTSHIFT		4		/* bit position of LASTMASK "flag" */
 #define	RES_GETLAST(res) 	(((res)._flags & RES_F_LASTMASK) >> RES_F_LASTSHIFT)
 
 /*
@@ -118,8 +118,8 @@ typedef struct __res_state_ext 		*res_state_ext;
 #define RES_PRIMARY		0x00000010		/* query primary server only */
 #define RES_IGNTC		0x00000020		/* ignore trucation errors */
 #define RES_RECURSE		0x00000040		/* recursion desired */
-#define RES_DEFNAMES	0x00000080		/* use default domain name */
-#define RES_STAYOPEN	0x00000100		/* Keep TCP socket open */
+#define RES_DEFNAMES		0x00000080		/* use default domain name */
+#define RES_STAYOPEN		0x00000100		/* Keep TCP socket open */
 #define RES_DNSRCH		0x00000200		/* search up local domain tree */
 
 #define RES_ROTATE		0x00004000		/* rotate ns list after each query */
@@ -133,11 +133,11 @@ typedef struct __res_state_ext 		*res_state_ext;
 #define RES_PRF_STATS		0x0001
 /*				0x0002	*/
 #define RES_PRF_CLASS   	0x0004
-#define RES_PRF_CMD			0x0008
+#define RES_PRF_CMD		0x0008
 #define RES_PRF_QUES		0x0010
-#define RES_PRF_ANS			0x0020
+#define RES_PRF_ANS		0x0020
 #define RES_PRF_AUTH		0x0040
-#define RES_PRF_ADD			0x0080
+#define RES_PRF_ADD		0x0080
 #define RES_PRF_HEAD1		0x0100
 #define RES_PRF_HEAD2		0x0200
 #define RES_PRF_TTLID		0x0400
@@ -165,19 +165,20 @@ extern struct __res_state _res;
 
 __BEGIN_DECLS
 void 		fp_resstat(res_state, FILE *);
-void 		fp_query(char *, FILE *);
+void 		fp_query(const u_char *, FILE *);
 char 		*hostalias(const char *);
 void 		putlong(u_long, u_char *);
 void 		putshort(u_short, u_char *);
 u_long 		getlong(const u_char *);
 u_short		getshort(const u_char *);
 
-void 		p_query(char *);
-const u_char *p_cdname(const u_char *, const u_char *, FILE *);
-char 		*p_rr(char *, char *, FILE *);
+void 		p_query(const u_char *);
+const u_char 	*p_cdname(const u_char *, const u_char *, FILE *);
+const u_char 	*p_fqname(const u_char *, const u_char *, FILE *);
+const u_char 	*p_rr(const u_char *, const u_char *, FILE *);
 const char	*p_type(int);
 const char	*p_class(int);
-const char  *p_option(u_int);
+const char  	*p_option(u_int);
 
 int		dn_skipname(const u_char *, const u_char *);
 int		dn_comp(const u_char *, u_char *, int, u_char **, u_char **);
@@ -197,7 +198,7 @@ int	 	res_query(char *, int, int, u_char *, int);
 int	 	res_querydomain(char *, char *, int, int, u_char *, int);
 int	 	res_search(char *, int, int, u_char *, int);
 int	 	res_send(const u_char *, int, u_char *, int);
-void    res_close(void);
-void    res_destroy(void);
+void    	res_close(void);
+void    	res_destroy(void);
 __END_DECLS
 #endif /* !_RESOLV_H_ */
