3aaa3926372dda73760de19eca4062857e5575c3
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-11-26 16:06:37 +1100

    Update thread-libc.c

--- lib/libc/thread/thread-libc.c
+++ lib/libc/thread/thread-libc.c
@@ -38,17 +38,16 @@
  */
 
 #ifdef _REENTRANT
-
 #include "namespace.h"
-#include "reentrant.h"
 
 #include <sys/types.h>
-#include <sys/signal.h>
 #include <sys/time.h>
-#include <sys/errno.h>
 #include <sys/syscall.h>
 
+#include <errno.h>
 #include <fcntl.h>
+
+#include "thread_private.h"
 
 extern int	__syscall(quad_t, ...);
 
