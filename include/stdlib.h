60ab3d5650a723484d8a227297021680d3994f40
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-11-29 15:49:02 +1100

    Update stdlib.h

--- include/stdlib.h
+++ include/stdlib.h
@@ -105,9 +105,9 @@ __BEGIN_DECLS
 void 	_Exit(int) __attribute__((__noreturn__));
 void 	abort(void) __attribute__((__noreturn__));
 int 	abs(int);
-int	 	atexit(void (*)(void));
+int	atexit(void (*)(void));
 double	atof(const char *);
-int	 	atoi(const char *);
+int	atoi(const char *);
 long	atol(const char *);
 #ifndef __BSEARCH_DECLARED
 #define __BSEARCH_DECLARED
@@ -123,14 +123,14 @@ long 	labs(long);
 ldiv_t	ldiv(long, long);
 void	*malloc(size_t);
 void	qsort(void *, size_t, size_t, int (*)(const void *, const void *));
-int	 	rand(void);
+int	rand(void);
 void	*realloc(void *, size_t);
 void	*reallocarray(void *, size_t, size_t);
 void	srand(unsigned);
 double	strtod(const char * __restrict, char ** __restrict);
 long	strtol(const char * __restrict, char ** __restrict, int);
 unsigned long strtoul(const char * __restrict, char ** __restrict, int);
-int	 	system(const char *);
+int	 system(const char *);
 
 /* Floating output conversion */
 char	*ecvt(double, int, int *, int *);
@@ -144,8 +144,8 @@ void 	l3tol(long *, char *, int);
 
 int	 	mblen(const char *, size_t);
 size_t	mbstowcs(wchar_t * __restrict, const char * __restrict, size_t);
-int	 	wctomb(char *, wchar_t);
-int	 	mbtowc(wchar_t * __restrict, const char * __restrict, size_t);
+int	wctomb(char *, wchar_t);
+int	mbtowc(wchar_t * __restrict, const char * __restrict, size_t);
 size_t	wcstombs(char * __restrict, const wchar_t * __restrict, size_t);
 
 #if defined(_POSIX_C_SOURCE) || defined(_XOPEN_SOURCE) || defined(__BSD_VISIBLE)
@@ -154,7 +154,7 @@ size_t	wcstombs(char * __restrict, const wchar_t * __restrict, size_t);
  * IEEE Std 1003.1c-95, also adopted by X/Open CAE Spec Issue 5 Version 2
  */
 #if (_POSIX_C_SOURCE - 0) >= 199506L || (_XOPEN_SOURCE - 0) >= 500 || defined(_REENTRANT) || defined(__BSD_VISIBLE)
-int	 	rand_r(unsigned int *);
+int	 rand_r(unsigned int *);
 #endif
 
 /*
@@ -171,7 +171,7 @@ long	 nrand48(unsigned short[3]);
 unsigned short *seed48(unsigned short[3]);
 void	 srand48(long);
 
-int	 	 putenv(const char *);
+int	 putenv(const char *);
 
 #endif
 
@@ -187,20 +187,17 @@ long	 random(void);
 char	 *setstate(char *);
 void	 srandom(unsigned long);
 
-char	*mkdtemp(char *);
-int	 	mkstemp(char *);
+char	 *mkdtemp(char *);
+int	 mkstemp(char *);
 #ifdef __MKTEMP_OK__
 	__RENAME(_mktemp)
 #endif
 	;
-
-int	 	setkey(const char *);
-
-char	*realpath(const char *, char *);
-
-int	 	ttyslot(void);
-
-void	*valloc(size_t);		/* obsoleted by malloc() */
+int	 setkey(const char *);
+char	 *realpath(const char *, char *);
+int	 ttyslot(void);
+void	 *valloc(size_t);		/* obsoleted by malloc() */
+int	 getsubopt(char **, char * const *, char **);
 #endif
 
 /*
@@ -212,20 +209,20 @@ long long int	atoll(const char *);
 /* LONGLONG */
 long long int	llabs(long long int);
 /* LONGLONG */
-lldiv_t			lldiv(long long int, long long int);
+lldiv_t		lldiv(long long int, long long int);
 /* LONGLONG */
 long long int	strtoll(const char * __restrict, char ** __restrict, int);
 /* LONGLONG */
 unsigned long long int
-				strtoull(const char * __restrict, char ** __restrict, int);
+		strtoull(const char * __restrict, char ** __restrict, int);
 #endif
 
 /*
  * The Open Group Base Specifications, Issue 6; IEEE Std 1003.1-2001 (POSIX)
  */
 #if (_POSIX_C_SOURCE - 0) >= 200112L || (_XOPEN_SOURCE - 0) >= 600 || defined(__BSD_VISIBLE)
-int	 	setenv(const char *, const char *, int);
-int	 	unsetenv(const char *);
+int	 setenv(const char *, const char *, int);
+int	 unsetenv(const char *);
 #endif
 
 /*
@@ -233,42 +230,42 @@ int	 	unsetenv(const char *);
  */
 #if defined(__BSD_VISIBLE)
 #if defined(alloca) && (alloca == __builtin_alloca) && (__GNUC__ < 2)
-void		*alloca(int);     /* built-in for gcc */
+void	*alloca(int);     /* built-in for gcc */
 #else
-void		*alloca(size_t);
+void	*alloca(size_t);
 #endif /* __GNUC__ */
 
-char	*getbsize(int *, long *);
-char	*cgetcap(char *, const char *, int);
-int	 	cgetclose(void);
-int	 	cgetent(char **, char **, const char *);
-int	 	cgetfirst(char **, char **);
-int	 	cgetmatch(const char *, const char *);
-int	 	cgetnext(char **, char **);
-int	 	cgetnum(char *, const char *, long *);
-int	 	cgetset(const char *);
-int	 	cgetstr(char *, const char *, char **);
-int	 	cgetustr(char *, const char *, char **);
-void    csetexpandtc(int);
+char	 *getbsize(int *, long *);
+char	 *cgetcap(char *, const char *, int);
+int	 cgetclose(void);
+int	 cgetent(char **, char **, const char *);
+int	 cgetfirst(char **, char **);
+int	 cgetmatch(const char *, const char *);
+int	 cgetnext(char **, char **);
+int	 cgetnum(char *, const char *, long *);
+int	 cgetset(const char *);
+int	 cgetstr(char *, const char *, char **);
+int	 cgetustr(char *, const char *, char **);
+void     csetexpandtc(int);
 
-int	 	daemon(int, int);
-char 	*devname(dev_t, mode_t);
-int	 	getloadavg(double [], int);
+int	 daemon(int, int);
+char 	 *devname(dev_t, mode_t);
+int	 getloadavg(double [], int);
 
 ssize_t	 hmac(const char *, const void *, size_t, const void *, size_t, void *, size_t);
 
 void	cfree(void *);
 
-int	 	heapsort(void *, size_t, size_t, int (*)(const void *, const void *));
-int	 	mergesort (void *, size_t, size_t, int (*)(const void *, const void *));
-int	 	radixsort(const unsigned char **, int, const unsigned char *, unsigned);
-int	 	sradixsort(const unsigned char **, int, const unsigned char *, unsigned);
+int	heapsort(void *, size_t, size_t, int (*)(const void *, const void *));
+int	mergesort (void *, size_t, size_t, int (*)(const void *, const void *));
+int	radixsort(const unsigned char **, int, const unsigned char *, unsigned);
+int	sradixsort(const unsigned char **, int, const unsigned char *, unsigned);
 
 void	 mi_vector_hash(const void * __restrict, size_t, uint32_t, uint32_t[3]);
 
 void	 setproctitle(const char *, ...) __attribute__((__format__(__printf__, 1, 2)));
 const char *getprogname(void) __attribute__((__const__));
-void		setprogname(const char *);
+void	setprogname(const char *);
 
 quad_t	 qabs(quad_t);
 qdiv_t	 qdiv(quad_t, quad_t);
@@ -300,11 +297,10 @@ typedef struct _locale *locale_t;
 #  endif
 
 #if defined(__BSD_VISIBLE)
-
-int	 	mblen_l(const char *, size_t, locale_t);
+int	mblen_l(const char *, size_t, locale_t);
 size_t	mbstowcs_l(wchar_t * __restrict, const char * __restrict, size_t, locale_t);
-int	 	wctomb_l(char *, wchar_t, locale_t);
-int	 	mbtowc_l(wchar_t * __restrict, const char * __restrict, size_t, locale_t);
+int	wctomb_l(char *, wchar_t, locale_t);
+int	mbtowc_l(wchar_t * __restrict, const char * __restrict, size_t, locale_t);
 size_t	wcstombs_l(char * __restrict, const wchar_t * __restrict, size_t, locale_t);
 #endif /* __BSD_VISIBLE */
 #endif /* _POSIX_C_SOURCE >= 200809 || __BSD_VISIBLE */
