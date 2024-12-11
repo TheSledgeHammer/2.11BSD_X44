0af9e5dd79b99c3858f22f7b260a346ab8380fab
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-12-11 14:14:47 +1100

    Update elf_note.h

--- lib/csu/common/elf_note.h
+++ lib/csu/common/elf_note.h
@@ -1,13 +1,11 @@
 #ifndef _ELF_NOTE_H_
 #define _ELF_NOTE_H_
 
-#include <sys/param.h>
-
 #define	CSU_ELF_NOTE_211BSD_NAMESZ		  7
 #define	CSU_ELF_NOTE_211BSD_DESCSZ		  4
 #define	CSU_ELF_NOTE_TYPE_211BSD_TAG	  1
 #define	CSU_ELF_NOTE_211BSD_NAME		    "NetBSD\0\0"
-#define	CSU_211BSD_Version__		        __211BSD_Version__
+#define	CSU_211BSD_Version__		        0000000100
 
 #define	CSU_ELF_NOTE_PAX_NAMESZ		      4
 #define	CSU_ELF_NOTE_PAX_DESCSZ		      4
