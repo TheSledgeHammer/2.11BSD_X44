48873bb1cf657d6a00f467aa106a2cfacb2cf1d9
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2024-12-11 12:02:50 +1100

    Update elf_note.h

--- lib/csu/common/elf_note.h
+++ lib/csu/common/elf_note.h
@@ -3,15 +3,15 @@
 
 #include <sys/param.h>
 
-#define	CSU_ELF_NOTE_211BSD_NAMESZ		7
-#define	CSU_ELF_NOTE_211BSD_DESCSZ		4
-#define	CSU_ELF_NOTE_TYPE_211BSD_TAG	1
-#define	CSU_ELF_NOTE_211BSD_NAME		"NetBSD\0\0"
-#define	CSU_211BSD_Version__		    __211BSD_Version__
+#define	CSU_ELF_NOTE_211BSD_NAMESZ		  7
+#define	CSU_ELF_NOTE_211BSD_DESCSZ		  4
+#define	CSU_ELF_NOTE_TYPE_211BSD_TAG	  1
+#define	CSU_ELF_NOTE_211BSD_NAME		    "NetBSD\0\0"
+#define	CSU_211BSD_Version__		        __211BSD_Version__
 
-#define	CSU_ELF_NOTE_PAX_NAMESZ		    4
-#define	CSU_ELF_NOTE_PAX_DESCSZ		    4
-#define	CSU_ELF_NOTE_TYPE_PAX_TAG	    3
-#define	CSU_ELF_NOTE_PAX_NAME		    "PaX\0"
+#define	CSU_ELF_NOTE_PAX_NAMESZ		      4
+#define	CSU_ELF_NOTE_PAX_DESCSZ		      4
+#define	CSU_ELF_NOTE_TYPE_PAX_TAG	      3
+#define	CSU_ELF_NOTE_PAX_NAME		        "PaX\0"
 
 #endif
