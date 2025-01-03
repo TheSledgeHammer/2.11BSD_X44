fbb7309db6fb29004244a34caf659c842bca3a64
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2025-01-03 20:12:27 +1100

    Update ansi.h

--- sys/sys/ansi.h
+++ sys/sys/ansi.h
@@ -45,12 +45,12 @@
 
 typedef unsigned long			__fixpt_t;	/* fixed point number */
 typedef	unsigned short			__nlink_t;	/* link count */
-typedef	long				__segsz_t;	/* segment size */
+//typedef	long				__segsz_t;	/* segment size */
 typedef long				__daddr_t;	/* disk address */
 typedef char 				*__caddr_t;	/* core address */
 typedef unsigned long			__ino_t;	/* inode number*/
 typedef	long				__swblk_t;	/* swap offset */
-typedef	long				__time_t;	/* time? */
+//typedef	long				__time_t;	/* time? */
 typedef unsigned long			__dev_t;	/* device number */
 typedef long				__memaddr_t;	/* core & swap address */
 typedef __uint32_t			__gid_t;	/* group id */
