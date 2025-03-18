7e06a42b646044831bb96f3776439bbae65fbd7f
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2025-03-18 14:35:38 +1100

    Update signal.h

--- sys/sys/signal.h
+++ sys/sys/signal.h
@@ -96,6 +96,7 @@
 #define	SIG_DFL		((void (*) (int))  0)
 #define	SIG_ERR		((void (*) (int)) -1)
 #define	SIG_IGN		((void (*) (int))  1)
+#define	SIG_CATCH	((void (*) (int))  2)
 #define	SIG_HOLD	((void (*) (int))  3)
 
 typedef unsigned long 	sigset_t;
