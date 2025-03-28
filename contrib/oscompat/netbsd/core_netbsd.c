f652a7315730b95772d2e009a006a6a72ef6cc75
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2025-03-28 15:31:52 +1100

    oscompat netbsd tweaks

---
+++ contrib/oscompat/netbsd/core_netbsd.c
@@ -0,0 +1,97 @@
+/*	$NetBSD: core_netbsd.c,v 1.24 2019/11/20 19:37:53 pgoyette Exp $	*/
+
+/*
+ * Copyright (c) 1997 Charles D. Cranor and Washington University.
+ * Copyright (c) 1991, 1993 The Regents of the University of California.
+ * Copyright (c) 1988 University of Utah.
+ *
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * from: Utah $Hdr: vm_unix.c 1.1 89/11/07$
+ *      @(#)vm_unix.c   8.1 (Berkeley) 6/11/93
+ * from: NetBSD: uvm_unix.c,v 1.25 2001/11/10 07:37:01 lukem Exp
+ */
+
+/* Dummy file */
+
+/*
+ * core_netbsd.c: Support for the historic NetBSD core file format.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: core_netbsd.c,v 1.24 2019/11/20 19:37:53 pgoyette Exp $");
+
+#include <sys/param.h>
+
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "core.h"
+
+#ifndef CORENAME
+#define	CORENAME(x)	x
+#endif
+
+struct coredump_state {
+	struct coredump_iostate *iocookie;
+	struct CORENAME(core) core;
+};
+
+static int coredump_internal(char *, struct siginfo *, struct coredump_state *, struct coredump_iostate *);
+int coredump_netbsd(void *, struct coredump_iostate *);
+
+static int
+coredump_internal(char *name, struct siginfo *sig, struct coredump_state *cs, struct coredump_iostate *iocookie)
+{
+    if (cs == NULL) {
+        cs = (struct coredump_state *)malloc(sizeof(*cs));
+    }
+	cs->iocookie = iocookie;
+	cs->core.c_midmag = 0;
+    if (name != NULL) {
+        strncpy(cs->core.c_name, name, MAXCOMLEN);
+    }
+	cs->core.c_nseg = 0;
+    cs->core.c_signo = sig->si_signo;
+	cs->core.c_ucode = sig->si_code;
+	cs->core.c_cpusize = 0;
+
+    return (0);
+}
+
+int
+coredump_netbsd(void *arg, struct coredump_iostate *iocookie)
+{
+    struct coredump_state cs;
+
+    return (coredump_internal(NULL, (struct siginfo *)arg, &cs, iocookie));
+}
