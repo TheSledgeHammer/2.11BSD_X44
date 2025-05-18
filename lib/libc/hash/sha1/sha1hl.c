7b7d22ff8c63c84e0a3dd8a0c279da48bbe3329d
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2025-05-18 20:54:57 +1000

    Update sha1hl.c

--- lib/libc/hash/sha1/sha1hl.c
+++ lib/libc/hash/sha1/sha1hl.c
@@ -10,15 +10,7 @@
 #if defined(LIBC_SCCS) && !defined(lint)
 __RCSID("$NetBSD: sha1hl.c,v 1.4 2008/04/13 02:04:32 dholland Exp $");
 #endif /* LIBC_SCCS and not lint */
-/*
-#include "namespace.h"
 
-#if HAVE_NBTOOL_CONFIG_H
-#include <sha1.h>
-#else
-#include <hash/sha1.h>
-#endif
-*/
 #define	HASH_ALGORITHM	SHA1
 
 #if HAVE_NBTOOL_CONFIG_H
