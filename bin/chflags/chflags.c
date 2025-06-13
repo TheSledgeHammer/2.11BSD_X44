720dfa0069cc660401c4d5fe0870bb75e8b84f7f
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2025-06-13 22:30:04 +1000

    Update chflags.c

--- bin/chflags/chflags.c
+++ bin/chflags/chflags.c
@@ -1,4 +1,36 @@
 /*
+ * Copyright (c) 1992, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+/*
  *	Program Name:   symcompact.c
  *	Date: December 3, 1994
  *	Author: S.M. Schultz
@@ -7,6 +39,19 @@
  *      Version Date            Reason For Modification
  *      1.0     03Dec94         1. Initial release into the public domain.
 */
+#if !defined(lint)
+#if 0
+static char copyright[] =
+"@(#) Copyright (c) 1992, 1993, 1994\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif
+#endif /* not lint */
+
+#if !defined(lint)
+#if 0
+static char sccsid[] = "@(#)chflags.c	8.5 (Berkeley) 4/1/94";
+#endif
+#endif /* not lint */
 
 #include <sys/cdefs.h>
 #include <sys/dir.h>
@@ -161,7 +206,6 @@ fts_option(void)
 		if (lflag) {
 			fts_options &= ~FTS_PHYSICAL;
 			fts_options |= FTS_LOGICAL;
-
 		}
 	}
 	return (fts_options);
@@ -278,13 +322,13 @@ recurse(struct stat *stp, char *dir, int savedir)
 void
 die(const char *fmt, ...)
 {
-    va_list	ap;
+	va_list ap;
 
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
 	fputc('\n', stderr);
 	va_end(ap);
-    exit(1);
+	exit(1);
 }
 
 int
