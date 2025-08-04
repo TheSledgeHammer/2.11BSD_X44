a0fab55e0ee11bd1e6a1f2b1283845efba72c448
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2025-08-04 13:44:49 +1000

    Update resource.h

--- sys/sys/resource.h
+++ sys/sys/resource.h
@@ -112,8 +112,10 @@ struct	k_rusage {					/* KERNEL RUSAGE STRUCTURE */
 #define	RLIMIT_MEMLOCK	6		/* locked-in-memory address space */
 #define RLIMIT_NPROC	7		/* number of processes */
 #define	RLIMIT_NOFILE	8		/* number of open files */
+#define	RLIMIT_SBSIZE	9		/* maximum size of all socket buffers */
+#define	RLIMIT_AS	10		/* virtual process size (inclusive of mmap) */
 
-#define	RLIM_NLIMITS	9		/* number of resource limits */
+#define	RLIM_NLIMITS	11		/* number of resource limits */
 
 #define	RLIM_INFINITY	(((u_quad_t)1 << 63) - 1)
 
