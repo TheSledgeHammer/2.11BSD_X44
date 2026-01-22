eec8ad63dd1f6b85af0c44c4c407db6d1944b61d
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2026-01-22 22:17:17 +1100

    added contrib/tcsh Makefiles and config
    - tcsh sources will be added soon

---
+++ contrib/tcsh/include/patchlevel.h
@@ -0,0 +1,13 @@
+/*
+ * patchlevel.h: Our life story.
+ */
+#ifndef _h_patchlevel
+#define _h_patchlevel
+
+#define ORIGIN "Astron"
+#define REV 6
+#define VERS 24
+#define PATCHLEVEL 16
+#define DATE "2025-07-09"
+
+#endif /* _h_patchlevel */
