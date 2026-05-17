a2d62e94c722e5fe24d2b6ae8564d8b0d370de75
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2026-05-17 14:15:09 +1000

    Update vm_mmap.c

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -684,12 +684,13 @@ mincore()
 	p = u.u_procp;
 	map = &p->p_vmspace->vm_map;
 	addr = (vm_offset_t)SCARG(uap, addr);
-	size = (vm_size_t)SCARG(uap, len);
+	size = round_page((vm_size_t)SCARG(uap, len));
 	vec = SCARG(uap, vec);
 	offset = (addr + vm_map_min(map));
 	if ((addr & PAGE_MASK) || ((addr + size) < addr)) {
 		return (ENOMEM);
 	}
+
 	error = vm_mincore(map, addr, size, offset, vec);
 	return (error == KERN_SUCCESS ? 0 : ENOMEM);
 	//return (EOPNOTSUPP);
@@ -1025,12 +1026,12 @@ out:
  * Find mincore entries from Segments and pages.
  */
 static int
-vm_mincore_segment_page(vm_map_entry_t entry, vm_amap_t amap, vm_object_t object, vm_size_t size, vm_offset_t offset)
+vm_mincore_segment_page(vm_map_entry_t entry, vm_amap_t amap, vm_object_t object, vm_size_t size, vm_offset_t offset, char *vec)
 {
 	vm_anon_t anon;
 	vm_segment_t segment;
 	vm_page_t page;
-	int sgi, pgi;
+	char sgi, pgi;
 	int i, j;
 
 	if (amap != NULL) {
@@ -1063,6 +1064,7 @@ vm_mincore_segment_page(vm_map_entry_t entry, vm_amap_t amap, vm_object_t object
 				sgi = 1;
 			}
 		}
+		*vec = sgi;
 
 		if ((segment != NULL) && (sgi != 0)) {
 			for (j = 0; j < round_page(size); j += PAGE_SIZE) {
@@ -1088,6 +1090,7 @@ vm_mincore_segment_page(vm_map_entry_t entry, vm_amap_t amap, vm_object_t object
 						pgi = 1;
 					}
 				}
+				*vec = pgi;
 			}
 		}
 	}
@@ -1099,11 +1102,13 @@ vm_mincore_segment_page(vm_map_entry_t entry, vm_amap_t amap, vm_object_t object
 	}
 
 	/* check for segment and page inconsistencies */
-	if (sgi < pgi) {
+	if ((sgi < pgi) || ((sgi == 0) && (pgi == 0))) {
 		return (0);
 	}
-	return (pgi);
+	return (1);
 }
+
+#define MINCORE_CHUNK 256 /* number of pages to process at once */
 
 /*
  * Internal version of mincore.
@@ -1117,22 +1122,29 @@ vm_mincore(vm_map_t map, vm_offset_t addr, vm_size_t len, vm_offset_t offset, ch
 	vm_amap_t amap;
 	vm_offset_t estart, eend, eaddr, eoffset;
 	vm_size_t elen;
-	int error, npages, vecindex;
+	vm_offset_t npages, pgoff;
+	vm_size_t pglen;
+	int error, retries;
 	char *evec;
+	bool_t isentry;
 
+	retries = 0;
+
+retry:
 	vm_map_lock_read(map);
-	if (vm_map_lookup_entry(map, addr, &entry)) {
+	isentry = vm_map_lookup_entry(map, addr, &entry);
+	if (isentry == TRUE) {
 		estart = entry->start;
 		eend = entry->end;
 		eoffset = entry->offset;
 		elen = (eend - estart);
-		eaddr = ((estart + elen) - elen);
+		eaddr = round_page(((estart + elen) - elen));
 
-		if (elen != len) {
+		if (eaddr != addr) {
 			error = ENOMEM;
 			goto out;
 		}
-		if (eaddr != addr) {
+		if (elen != len) {
 			error = ENOMEM;
 			goto out;
 		}
@@ -1140,33 +1152,38 @@ vm_mincore(vm_map_t map, vm_offset_t addr, vm_size_t len, vm_offset_t offset, ch
 			error = ENOMEM;
 			goto out;
 		}
-		if ((entry->object.vm_object == NULL)
-				&& (entry->aref.ar_amap == NULL)) {
-			error = ENOMEM;
-			goto out;
-		}
 		amap = entry->aref.ar_amap;			/* top layer */
 		object = entry->object.vm_object;	/* bottom layer */
-
-		error = vm_mincore_segment_page(entry, amap, object, elen, eoffset);
-		if (error != 0) {
-			*evec = (char)error;
-			npages = atop(eaddr - eoffset);
-			for (vecindex = 0; vecindex < npages; vecindex++) {
-				/* found vec */
-				if (evec == vec) {
-					error = KERN_SUCCESS;
-					goto out;
-				} else {
-					error = ENOMEM;
-					goto out;
-				}
-			}
-		} else {
-			/* Nothing was found */
+		if ((object == NULL) && (amap == NULL)) {
 			error = ENOMEM;
 			goto out;
 		}
+		error = vm_mincore_segment_page(entry, amap, object, elen, eoffset, evec);
+		if (error == 0) {
+			error = ENOMEM;
+			goto out;
+		}
+		pgoff = 0;
+		pglen = (elen / PAGE_SIZE);
+		while (pgoff < pglen) {
+			npages = MIN((pglen - pgoff), MINCORE_CHUNK);
+			error = copyout(evec, (vec + pgoff), npages);
+			if (error != 0) {
+				error = ENOMEM;
+				goto out;
+			}
+			pgoff += npages;
+		}
+		error = KERN_SUCCESS;
+		goto out;
+	} else {
+		entry = CIRCLEQ_NEXT(entry, cl_entry);
+		if (retries == 4) {
+			error = ENOMEM;
+			goto out;
+		}
+		retries++;
+		goto retry;
 	}
 
 out:
