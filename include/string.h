6f8a1995779201e0515d3f585e0f683556ca813f
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2026-05-18 18:51:37 +1000

    Add memrchr function declaration to string.h

--- include/string.h
+++ include/string.h
@@ -57,6 +57,7 @@ typedef	_BSD_SIZE_T_	size_t;
 #include <sys/cdefs.h>
 __BEGIN_DECLS
 void	*memchr(const void *, int, size_t);
+void	*memrchr(const void *, int, size_t);
 int	 	memcmp(const void *, const void *, size_t);
 void	*memcpy(void * __restrict, const void * __restrict, size_t);
 void	*memmove(void *, const void *, size_t);
