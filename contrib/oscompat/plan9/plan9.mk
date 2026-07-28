e02c536d606f5a2f52c5136adf0fd842e057e862
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2026-07-28 16:38:09 +1000

    contrib/oscompat/plan9 Makefile updates

--- contrib/oscompat/plan9/plan9.mk
+++ contrib/oscompat/plan9/plan9.mk
@@ -5,25 +5,3 @@
 # those dependencies have been disabled or enabled
 #
 
-MKLIBFMT?= no
-MKLIBUTF?= no
-MKLIBBIO?= no
-MKLIBREGEXP?= no
-MKMK?= no # See Notes/Info below for issues
-
-# 
-# Notes/Info:
-# 
-# libutf: 
-#	- Plan 9 UTF7 library
-# libfmt:
-# 	- Plan 9 format library
-# libbio:
-# 	- Plan 9 buffered io library
-# libregexp:
-# 	- Plan 9 regular expression library
-# mk:
-# 	- Plan 9 make program
-#	- Issues: Unresovled compile issues, 
-# 	which prevent it compiling.
-#
