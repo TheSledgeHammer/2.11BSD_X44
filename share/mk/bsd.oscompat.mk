e92821e65c276c314598059e292e982585a9e49a
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2026-07-28 16:40:01 +1000

    share/mk/bsd.oscompat.mk fixes

--- share/mk/bsd.oscompat.mk
+++ share/mk/bsd.oscompat.mk
@@ -9,92 +9,44 @@ _BSD_OSCOMPAT_MK_=1
 # 2.11BSD Compatability
 MK211BSD= yes
 
-.if(${MK211BSD} != "no")
-SUBDIR+= 211bsd
+# DragonflyBSD Compatability
+MKDRAGONFLYBSD=	no
+
+# FreeBSD Compatability
+MKFREEBSD= no
+
+# NetBSD Compatability
+MKNETBSD= yes
+
+# OpenBSD Compatability
+MKOPENBSD= no
+
+# Plan9 Compatability
+MKPLAN9= yes
+
+# Linux Compatability
+MKLINUX= no
+
+# Solaris Compatability
+MKSOLARIS= no
+
+# OS Compatability options which default to "yes"
 
 # 2.11BSD options which default to "yes"
 _MK211BSD.yes= \
 	MKLIBSTUBS
 
-.for var in ${_MK211BSD.yes}
-${var}?=	${${var}:Uyes}
-.endfor
-
-# 2.11BSD options which default to "no"
-_MK211BSD.no= \
-	MKLIBNDBM \
-	MKLIBMP \
-	MKLIBOM \
-	MKLIBTERMCAP \
-	MKLIBVMF
-	
-.for var in ${_MK211BSD.no}
-${var}?=	${${var}:Uno}
-.endfor
-
-.endif
-
-# DragonflyBSD Compatability
-MKDRAGONFLYBSD=	no
-
-.if(${MKDRAGONFLYBSD} != "no")
-SUBDIR+= dragonflybsd
-
 # DragonflyBSD options which default to "yes"
 _MKDRAGONFLYBSD.yes=
-
-# DragonflyBSD options which default to "no"
-_MKDRAGONFLYBSD.no=
-
-.endif
-
-# FreeBSD Compatability
-MKFREEBSD= no
-
-.if(${MKFREEBSD} != "no")
-SUBDIR+= freebsd
 
 # FreeBSD options which default to "yes"
 _MKFREEBSD.yes=
 
-# FreeBSD options which default to "no"
-_MKFREEBSD.no=
-
-.endif
-
-# NetBSD Compatability
-MKNETBSD= yes
-
-.if(${MKNETBSD} != "no")
-SUBDIR+= netbsd
-
 # NetBSD options which default to "yes"
 _MKNETBSD.yes=
 
-# NetBSD options which default to "no"
-_MKNETBSD.no=
-
-.endif
-
-# OpenBSD Compatability
-MKOPENBSD= no
-
-.if(${MKOPENBSD} != "no")
-SUBDIR+= openbsd
-
 # OpenBSD options which default to "yes"
 _MKOPENBSD.yes=
-
-# OpenBSD options which default to "no"
-_MKOPENBSD.no=
-
-.endif
-
-# Plan9 Compatability
-MKPLAN9= yes
-
-.if(${MKPLAN9} != "no")
-SUBDIR+= plan9
 
 # Plan9 options which default to "yes"
 _MKPLAN9.yes= \
@@ -102,48 +54,73 @@ _MKPLAN9.yes= \
 	MKLIBFMT \
 	MKLIBREGEXP \
 	MKLIBUTF \
-	MKMK \
-
-.for var in ${_MKPLAN9.yes}
-${var}?=	${${var}:Uyes}
-.endfor
-
-# Plan9 options which default to "no"
-_MKPLAN9.no=
-
-.for var in ${_MKPLAN9.no}
-${var}?=	${${var}:Uno}
-.endfor
-
-.endif
-
-# Linux Compatability
-MKLINUX= no
-
-.if(${MKLINUX} != "no")
-SUBDIR+= linux
+	MKMK
 
 # Linux options which default to "yes"
 _MKLINUX.yes=
 
-# Linux options which default to "no"
-_MKLINUX.no=
-
-.endif
-
-# Solaris Compatability
-MKSOLARIS= no
-
-.if(${MKSOLARIS} != "no")
-SUBDIR+= solaris
-
 # Solaris options which default to "yes"
 _MKSOLARIS.yes=
+
+_MKVARS.yes= \
+    ${_MK211BSD.yes} \
+    ${_MKDRAGONFLYBSD.yes} \
+    ${_MKFREEBSD.yes} \
+    ${_MKNETBSD.yes} \
+    ${_MKOPENBSD.yes} \
+    ${_MKPLAN9.yes} \
+    ${_MKLINUX.yes} \
+    ${_MKSOLARIS.yes}
+
+.for var in ${_MKVARS.yes}
+${var}?=	${${var}.:Uyes}
+.endfor
+
+# OS Compatability options which default to "no"
+
+# 2.11BSD options which default to "no"
+_MK211BSD.no= \
+	MKLIBNDBM \
+    MKLIBFORTRAN \
+	MKLIBMP \
+	MKLIBOM \
+	MKLIBTERMCAP \
+	MKLIBVMF
+
+# DragonflyBSD options which default to "no"
+_MKDRAGONFLYBSD.no=
+
+# FreeBSD options which default to "no"
+_MKFREEBSD.no=
+
+# NetBSD options which default to "no"
+_MKNETBSD.no=
+
+# OpenBSD options which default to "no"
+_MKOPENBSD.no=
+
+# Plan9 options which default to "no"
+_MKPLAN9.no=
+
+# Linux options which default to "no"
+_MKLINUX.no=
 
 # Solaris options which default to "no"
 _MKSOLARIS.no=
 
-.endif
+_MKVARS.no= \
+    ${_MK211BSD.no} \
+    ${_MKDRAGONFLYBSD.no} \
+    ${_MKFREEBSD.no} \
+    ${_MKNETBSD.no} \
+    ${_MKOPENBSD.no} \
+    ${_MKPLAN9.no} \
+    ${_MKLINUX.no} \
+    ${_MKSOLARIS.no}
+
+.for var in ${_MKVARS.no}
+${var}?=	${${var}.:Uno}
+.endfor
 
 .include <bsd.sys.mk>
 
