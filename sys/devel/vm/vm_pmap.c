5c47ecda0cafb8b6051cee43408724fdef378ca8
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2026-08-01 17:01:58 +1000

    Update vm_pmap.c
    further revisions to estabur in devel/vm/vm_pmap.c
    - likely the version to be moved from devel into the kernel

--- sys/devel/vm/vm_pmap.c
+++ sys/devel/vm/vm_pmap.c
@@ -128,410 +128,150 @@ vm_pmap_bootstrap(vm_offset_t *data, vm_size_t map_size, unsigned long map_numbe
 
 #include "vm_idspace.h"
 
-/*
- * Based around 2.11BSD's phys system call
- * Setup u.uisa and u.uisd from pmap.
- */
-vm_offset_t uisd_tmp[NOVL], uisa_tmp[NOVL];
-
-int
-vm_pmap_lookup_phys(map, virt, phys, nsegs, size, start, end)
-	vm_map_t map;
-	vm_offset_t *virt, *phys, *nsegs;
-	vm_size_t size;
-	vm_offset_t start, end;
-{
-	pmap_t pmap;
-	vm_offset_t addr;
-
-	pmap = vm_map_pmap(map);
-	size = round_page(size);
-	if ((pmap == NULL) || ((end - start) < size)) {
-		return (1);
-	}
-	for (addr = trunc_page(start); addr < round_page(end); addr += PAGE_SIZE) {
-		if (addr == size) {
-			*nsegs = atos(addr);
-			*virt = addr;
-			*phys = pmap_extract(pmap, addr);
-			return (0);
-		}
-	}
-	return (1);
-}
-
-int
-vm_pmap_validate_phys(map, virt, phys, nsegs, size, start, end)
-	vm_map_t map;
-	vm_offset_t *virt, *phys, *nsegs;
-	vm_size_t size;
-	vm_offset_t start, end;
-{
-	vm_offset_t stso;
-	int segnum, error, num;
-
-
-
-	error = vm_pmap_lookup_phys(map, virt, phys, nsegs, size, start, end);
-	if (error != 0) {
-		return (error);
-	}
-	/* initialize temp uisa and uisd */
-	uisd_tmp[0] = 0;
-	uisa_tmp[0] = 0;
-
-	/* sanity check */
-	num = (int)*nsegs;
-	for (segnum = 0; segnum < num; segnum++) {
-		stso = segnum_to_segment_offset(segnum);
-		if ((stso * num) == *virt) {
-			uisd_tmp[segnum] = *virt;
-			uisa_tmp[segnum] = *phys;
-			return (0);
-		}
-	}
-	return (1);
-}
-
-void
-vm_pmap_phys(map, size, start, end)
+static int
+estabur_lookup(map, size, addr, desc, val, type, flags)
 	vm_map_t map;
 	vm_size_t size;
-	vm_offset_t start, end;
-{
-	pmap_t pmap;
-	vm_offset_t addr, virt, phys, num;
-	int segnum, error;
-
-	error = vm_pmap_validate_phys(map, &virt, &phys, &num, size, start, end);
-	if (error != 0) {
-		return;
-	}
-
-	u.u_uisd[0] = uisd_tmp[0];
-	u.u_uisa[0] = uisa_tmp[0];
-
-	for (segnum = 0; segnum < num; segnum++) {
-		u.u_uisd[segnum] = uisd_tmp[segnum];
-		u.u_uisa[segnum] = uisa_tmp[segnum];
-	}
-}
-
-void
-vm_idspace_entry_init_phys(entry)
-	vm_idspace_entry_t entry;
-{
-	vm_pmap_phys(entry->map, entry->size, entry->start, entry->end);
-}
-
-int
-vm_idspace_region_allocate(idspace, segnum)
-	vm_idspace_t idspace;
-	int segnum;
-{
-	vm_segment_region_t region;
-
-	region = vm_segment_region_alloc(idspace->mtype);
-	if (region != NULL) {
-		vm_segment_region_insert(idspace, region, segnum);
-		idspace->region = region;
-		return (0);
-	}
-	return (ENOMEM);
-}
-
-void
-vm_idspace_region_deallocate(idspace, segnum)
-	vm_idspace_t idspace;
-	int segnum;
-{
-	vm_segment_region_t region;
-
-	region = idspace->region;
-	if (region != NULL) {
-		vm_segment_region_remove(region, segnum);
-		if (TAILQ_EMPTY(&idspace->header)) {
-			vm_segment_region_free(region, idspace->mtype);
-			idspace->region = region;
-		}
-	}
-}
-
-int
-vm_idspace_region_read(idspace, segnum, addr, desc, flags, is_txt, is_ext, is_abs)
-	vm_idspace_t idspace;
-	int segnum, flags;
-	vm_offset_t addr, desc;
-	bool_t is_txt, is_ext, is_abs;
-{
-	vm_segment_region_t region;
-	int error;
-
-	region = idspace->region;
-	if (region != NULL) {
-		region->flags = flags;
-		region->is_text = is_txt;
-		region->is_extension = is_ext;
-		region->is_abs = is_abs;
-		error = vm_segment_register_read(region, segnum, addr, desc);
-		if (error != 0) {
-			return (error);
-		}
-		return (0);
-	}
-	return (1);
-}
-
-int
-vm_idspace_region_write(idspace, segnum, addr, desc, flags, is_txt, is_ext, is_abs)
-	vm_idspace_t idspace;
-	int segnum, flags;
-	vm_offset_t addr, desc;
-	bool_t is_txt, is_ext, is_abs;
-{
-	vm_segment_region_t region;
-	int error;
-
-	region = idspace->region;
-	if (region != NULL) {
-		region->flags = flags;
-		region->is_text = is_txt;
-		region->is_extension = is_ext;
-		region->is_abs = is_abs;
-		error = vm_segment_register_write(region, segnum, addr, desc);
-		if (error != 0) {
-			return (error);
-		}
-		return (0);
-	}
-	return (1);
-}
-
-int
-vm_idspace_map(idspace, idspacemap, val, size, segnum)
-	vm_idspace_t idspace;
-	vm_idspace_map_t idspacemap;
-	vm_offset_t val;
-	vm_size_t size;
-	int segnum;
-{
-	vm_map_t map;
-	vm_segment_region_t region;
-	int error;
-
-	error = vm_idspace_region_allocate(idspace, segnum);
-	if (error != 0) {
-		return (error);
-	}
-	region = idspace->region;
-	if (region != NULL) {
-		map = idspacemap->map;
-		if (map != NULL) {
-			/*
-			 * allocated val with kmem if is_alloced is false and val equals
-			 * 0.
-			 */
-			if ((idspacemap->is_alloced != TRUE) && (val == 0)) {
-				val = kmem_alloc_wait(map, size);
-				idspacemap->is_alloced = TRUE;
-			}
-		} else {
-			return (ENOMEM);
-		}
-		error = vm_idspace_map_check_protection(idspace, idspacemap);
-		if (error != 0) {
-			return (error);
-		}
-
-		return (0);
-	}
-	return (ENOMEM);
-}
-
-int
-vm_idspace_unmap(idspace, idspacemap, val, size, segnum)
-	vm_idspace_t idspace;
-	vm_idspace_map_t idspacemap;
-	vm_offset_t val;
-	vm_size_t size;
-	int segnum;
-{
-	vm_map_t map;
-	vm_segment_region_t region;
-	int error;
-
-	region = idspace->region;
-	if (region != NULL) {
-		map = idspacemap->map;
-		if (map != NULL) {
-			/*
-			 * free val from kmem if is_alloced is true and val is greater
-			 * than 0.
-			 */
-			if ((idspacemap->is_alloced != FALSE) && (val > 0)) {
-				kmem_free_wakeup(map, val, size);
-				idspacemap->is_alloced = FALSE;
-			}
-		} else {
-			return (ENOMEM);
-		}
-		error = vm_idspace_map_check_protection(idspace, idspacemap);
-		if (error != 0) {
-			return (error);
-		}
-		vm_idspace_region_deallocate(idspace, segnum);
-		return (0);
-	}
-	return (ENOMEM);
-}
-
-int
-vm_idspace_map_protect(idspacemap, region, set_max)
-	vm_idspace_map_t idspacemap;
-	vm_segment_region_t region;
-	bool_t	set_max;
-{
-	return (vm_map_protect(idspacemap->map, idspacemap->start, idspacemap->end,
-			region->protect, set_max));
-}
-
-int
-vm_idspace_map_check_protection(idspacemap, region)
-	vm_idspace_map_t idspacemap;
-	vm_segment_region_t region;
-{
-	return (vm_map_check_protection(idspacemap->map, idspacemap->start,
-			idspacemap->end, region->protect));
-}
-
-vm_offset_t
-estabur_lookup(addr, size)
-	vm_offset_t addr;
-	vm_size_t size;
-{
-	vm_offset_t val;
-
-	for (val = 0; val <= round_page(size); val += PAGE_SIZE) {
-		if (addr == val) {
-			return (addr);
-		}
-	}
-	return (0);
-}
-
-int
-estabur_validate(map, addr, desc, data, size, val, type, flags)
-	vm_map_t map;
 	vm_offset_t *addr, *desc;
-	vm_offset_t data;
-	vm_size_t size;
 	vm_offset_t val;
 	int type, flags;
 {
-	vm_offset_t virt, phys, num;
+	vm_offset_t virt, phys, num, data;
 	int error;
+
+	if (map == NULL) {
+		return (ENOMEM);
+	}
 
 	error = vm_pmap_validate_phys(map, &virt, &phys, &num, size, vm_map_min(map), vm_map_max(map));
 	if (error != 0) {
 		return (error);
 	}
 
-	if ((virt != desc) && (phys != addr)) {
-		return (ENOMEM);
+	/* check data size */
+	if (data < SEGMENT_SIZE) {
+		data = ptoa(num);
+	} else {
+		data = stoa(num);
 	}
 
-	while (data >= round_page(size)) {
+#define emask(x, y) (((x) - (y)) << 8)
+
+	while (size >= data) {
 		if (type == (PSEG_DATA | PSEG_TEXT)) {
-			*desc++ = (data | flags);
+			*desc++ = (emask(data, 1) | flags);
 			*addr++ = val;
-			val += size;
-			data -= size;
+			val += data;
+			data -= data;
 		} else {
-			val -= size;
-			data -= size;
-			*--desc = (data | flags);
+			val -= data;
+			data -= data;
+			*--desc = (emask(0, 0) | flags);
 			*--addr = val;
 		}
 	}
-	if (data) {
+	if (size) {
 		if (type == (PSEG_DATA | PSEG_TEXT)) {
-			*desc++ = (data | flags);
+			*desc++ = (emask(size, 1) | flags);
 			*addr++ = val;
 			if (type == PSEG_DATA) {
 				val += data;
 			}
 		} else {
-			*--desc = (data | flags);
-			*--addr = (val - size);
+			*--desc = (emask(data, size) | flags);
+			*--addr = (val - data);
 		}
 	}
+#undef emask
 	return (0);
 }
 
 /* text */
-int
-estabur_text(map, ntext, addr, desc, size, xrw)
+static int
+estabur_text(map, text, tsize, taddr, addr, desc, flags)
 	vm_map_t map;
-	vm_offset_t ntext;
+	vm_text_t text;
+	vm_size_t tsize;
+	caddr_t taddr;
 	vm_offset_t *addr, *desc;
-	vm_size_t size;
-	int xrw;
+	int flags;
 {
 	vm_offset_t val;
 	int error;
 
 	val = 0;
-	error = estabur_validate(map, addr, desc, ntext, size, val, PSEG_TEXT, (xrw | SEGM_TX));
+	if (text == NULL) {
+		return (ENOMEM);
+	}
+
+	error = estabur_lookup(map, tsize, addr, desc, val, PSEG_TEXT, flags);
 	if (error != 0) {
 		return (error);
 	}
+	TEXT_SEGMENT(text, tsize, taddr, flags);
 	return (0);
 }
 
 /* data */
-int
-estabur_data(map, ndata, addr, desc, size)
+static int
+estabur_data(map, data, dsize, daddr, addr, desc, flags)
 	vm_map_t map;
-	vm_offset_t ndata;
+	vm_data_t data;
+	vm_size_t dsize;
+	caddr_t daddr;
 	vm_offset_t *addr, *desc;
-	vm_size_t size;
+	int flags;
 {
 	vm_offset_t val;
 	int error;
 
 	val = 0;
-	error = estabur_validate(map, addr, desc, ndata, size, val, PSEG_DATA, SEGM_RW);
+	if (data == NULL) {
+		return (ENOMEM);
+	}
+
+	error = estabur_lookup(map, dsize, addr, desc, val, PSEG_DATA, flags);
 	if (error != 0) {
 		return (error);
 	}
+	DATA_SEGMENT(data, dsize, daddr, flags);
 	return (0);
 }
 
 /* stack */
-int
-estabur_stack(map, nstack, addr, desc, size)
+static int
+estabur_stack(map, stack, ssize, saddr, addr, desc, flags)
 	vm_map_t map;
-	vm_offset_t nstack;
+	vm_stack_t stack;
+	vm_size_t ssize;
+	caddr_t saddr;
 	vm_offset_t *addr, *desc;
-	vm_size_t size;
+	int flags;
 {
 	vm_offset_t val;
 	int error;
 
-	val = nstack;
-	error = estabur_validate(map, addr, desc, nstack, size, val, PSEG_STACK, (SEGM_RW | SEGM_ED));
+	val = ssize;
+	if (stack == NULL) {
+		return (ENOMEM);
+	}
+
+	error = estabur_lookup(map, ssize, saddr, addr, desc, val, PSEG_STACK, flags);
 	if (error != 0) {
 		return (error);
 	}
+	STACK_SEGMENT(stack, ssize, saddr, flags);
 	return (0);
 }
 
-int
-estabur(map, nt, nd, ns, sep, size, xrw)
+static int
+estabur(map, text, data, stack, tsize, dsize, ssize, sep, flags)
 	vm_map_t map;
-	vm_offset_t nt, nd, ns;
-	vm_size_t size;
-	int sep, xrw;
+	vm_text_t text;
+	vm_data_t data;
+	vm_stack_t stack;
+	segsz_t tsize, dsize, ssize;
+	int sep, flags;
 {
 	vm_offset_t *addr, *desc;
 	int error;
@@ -539,19 +279,21 @@ estabur(map, nt, nd, ns, sep, size, xrw)
 	addr = &u.u_uisa[0];
 	desc = &u.u_uisd[0];
 
-	error = estabur_text(map, nt, addr, desc, size, xrw);
+	error = estabur_text(map, text, (vm_size_t)tsize, text->psx_taddr, addr, desc, (flags | SEGM_TX));
 	if (error != 0) {
 		return (error);
 	}
 
+#ifndef NONSEPARATE
 	if (sep) {
 		while (addr < &u.u_uisa[8]) {
 			*addr++ = 0;
 			*desc++ = 0;
 		}
 	}
+#endif /* !NONSEPARATE */
 
-	error = estabur_data(map, nd, addr, desc, size);
+	error = estabur_data(map, data, (vm_size_t)dsize, data->psx_daddr, addr, desc, SEG_RW);
 	if (error != 0) {
 		return (error);
 	}
@@ -566,6 +308,7 @@ estabur(map, nt, nd, ns, sep, size, xrw)
 		*addr++ = 0;
 	}
 
+#ifndef NONSEPARATE
 	if (sep) {
 		while (addr < &u.u_uisa[16]) {
 			if (*desc & SEGM_ABS) {
@@ -577,12 +320,14 @@ estabur(map, nt, nd, ns, sep, size, xrw)
 			*addr++ = 0;
 		}
 	}
+#endif /* !NONSEPARATE */
 
-	error = estabur_stack(map, ns, addr, desc, size);
+	error = estabur_stack(map, stack, (vm_size_t)ssize, stack->psx_saddr, addr, desc, (SEG_RW | SEGM_ED));
 	if (error != 0) {
 		return (error);
 	}
 
+#ifndef NONSEPARATE
 	if (!sep) {
 		addr = &u.u_uisa[0];
 		desc = &u.u_uisa[8];
@@ -595,8 +340,33 @@ estabur(map, nt, nd, ns, sep, size, xrw)
 			*desc++ = *addr++;
 		}
 	}
+#endif /* !NONSEPARATE */
 
 	vm_sureg();
+	return (0);
+}
+
+int
+vm_estabur(p, tsize, dsize, ssize, sep, flags)
+	struct proc	*p;
+	segsz_t	 tsize, dsize, ssize;
+	int sep, flags;
+{
+	register struct vmspace *vm;
+	vm_map_t map;
+	vm_pseudo_segment_t	pseg;
+	int error;
+
+	vm = p->p_vmspace;
+	map = vm->vm_map;
+	pseg = &vm->vm_psegment;
+	if (pseg == NULL) {
+		return (ENOMEM);
+	}
+	error = estabur(map, pseg->ps_text, pseg->ps_data, pseg->ps_stack, tsize, dsize, ssize, sep, flags);
+	if (error != 0) {
+		return (error);
+	}
 	return (0);
 }
 
